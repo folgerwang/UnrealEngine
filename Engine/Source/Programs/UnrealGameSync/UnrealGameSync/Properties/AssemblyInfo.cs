cd13754ebb89f01da27b5e956d6d03d6b5ac8d5a
Ben Marsh <Ben.Marsh@epicgames.com>
2018-05-11 17:16:09 -0400

    UGS: Only update non-visible tabs every 10 minutes.
    #rb none
    
    [CL 4069146 by Ben Marsh in Dev-Core branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.134.*")]
-[assembly: AssemblyFileVersion("1.134.0.0")]
+[assembly: AssemblyVersion("1.135.*")]
+[assembly: AssemblyFileVersion("1.135.0.0")]
