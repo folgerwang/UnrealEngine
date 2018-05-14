1f4bc6cdbdc6a8439d73fa3cddf2f29f5cb97347
Ben Marsh <Ben.Marsh@epicgames.com>
2018-05-14 17:22:59 -0400

    Missing files.
    #rb none
    
    [CL 4071914 by Ben Marsh in Dev-Core branch]

--- Engine/Source/Programs/UnrealPak/UnrealPak.Build.cs
+++ Engine/Source/Programs/UnrealPak/UnrealPak.Build.cs
@@ -8,7 +8,7 @@ public class UnrealPak : ModuleRules
 	{
 		PublicIncludePaths.Add("Runtime/Launch/Public");
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "PakFile", "Json", "Projects", "ApplicationCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "PakFile", "Json", "Projects", "ApplicationCore", "PakFileUtilities" });
 
 		PrivateIncludePaths.Add("Runtime/Launch/Private");      // For LaunchEngineLoop.cpp include
 
