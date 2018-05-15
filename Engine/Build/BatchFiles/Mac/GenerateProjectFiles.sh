cb8facec9fbb5fa0759030546ce734a977428621
Brandon Schaefer <Brandon.Schaefer@epicgames.com>
2018-05-15 10:14:35 -0400

    Set our location to Mac/GenerateProject.sh if we are not running from that location
    Same fix for Linux here UE-57928
    
    #jira UE-59127
    #codereview Anthony.Bills
    #review-4072694 @Anthony.Bills
    #fyi Michael.Trepka
    #rb Anthony.Bills
    
    [CL 4072700 by Brandon Schaefer in Dev-Editor branch]

--- Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh
+++ Engine/Build/BatchFiles/Mac/GenerateProjectFiles.sh
@@ -4,23 +4,26 @@ echo
 echo Setting up Unreal Engine 4 project files...
 echo
 
+# If ran from somewhere other then the script location we'll have the full base path
+BASE_PATH="`dirname "$0"`"
+
 # this is located inside an extra 'Mac' path unlike the Windows variant.
 
-if [ ! -d ../../../Binaries/DotNET ]; then
+if [ ! -d "$BASE_PATH/../../../Binaries/DotNET" ]; then
  echo GenerateProjectFiles ERROR: It looks like you're missing some files that are required in order to generate projects.  Please check that you've downloaded and unpacked the engine source code, binaries, content and third-party dependencies before running this script.
  exit 1
 fi
 
-if [ ! -d ../../../Source ]; then
+if [ ! -d "$BASE_PATH/../../../Source" ]; then
  echo GenerateProjectFiles ERROR: This script file does not appear to be located inside the Engine/Build/BatchFiles/Mac directory.
  exit 1
 fi
 
-source SetupMono.sh "`dirname "$0"`"
+. "$BASE_PATH/SetupMono.sh" $BASE_PATH
 
-if [ -f ../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj ]; then
-	xbuild ../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj /property:Configuration="Development" /verbosity:quiet /nologo |grep -i error
+if [ -f "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" ]; then
+	xbuild "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" /property:Configuration="Development" /verbosity:quiet /nologo |grep -i error
 fi
 
 # pass all parameters to UBT
-mono ../../../Binaries/DotNET/UnrealBuildTool.exe -projectfiles "$@"
+mono "$BASE_PATH/../../../Binaries/DotNET/UnrealBuildTool.exe" -projectfiles "$@"
