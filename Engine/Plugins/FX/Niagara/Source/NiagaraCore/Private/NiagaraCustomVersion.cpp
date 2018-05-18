20750b09c6eed5b3f3cc61b776c870086b5dd7b9
Shaun Kime <Shaun.Kime@epicgames.com>
2018-05-18 15:57:19 -0400

    Updating the compile GUID because the previous change adjusted generated code
    #rb none
    #tests n/a
    
    [CL 4081111 by Shaun Kime in Dev-Niagara branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x375471F4, 0xD5DA466F, 0x8E78C8A5, 0xA257BCA2);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0000B90F, 0xD8A4445C, 0x9E951397, 0xC5499F26);
