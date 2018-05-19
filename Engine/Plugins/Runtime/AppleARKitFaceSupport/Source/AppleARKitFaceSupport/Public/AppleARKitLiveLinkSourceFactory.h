d8b88e625d0d45dbf0e86cd2f7310fab82a05505
Joe Graf <Joe.Graf@epicgames.com>
2018-05-19 15:04:49 -0400

    Fixed export issue that didn't show up on Mac
    #rb: none
    
    [CL 4082325 by Joe Graf in Dev-VR branch]

--- Engine/Plugins/Runtime/AppleARKitFaceSupport/Source/AppleARKitFaceSupport/Public/AppleARKitLiveLinkSourceFactory.h
+++ Engine/Plugins/Runtime/AppleARKitFaceSupport/Source/AppleARKitFaceSupport/Public/AppleARKitLiveLinkSourceFactory.h
@@ -8,21 +8,21 @@
 
 
 /** Interface that publishes face ar blend shape information */
-class APPLEARKIT_API IARKitBlendShapePublisher
+class APPLEARKITFACESUPPORT_API IARKitBlendShapePublisher
 {
 public:
 	virtual void PublishBlendShapes(FName SubjectName, double Timestamp, uint32 FrameNumber, const FARBlendShapeMap& BlendShapes) = 0;
 };
 
 /** Interface that publishes face ar blend shape information via LiveLink */
-class APPLEARKIT_API ILiveLinkSourceARKit :
+class APPLEARKITFACESUPPORT_API ILiveLinkSourceARKit :
 	public IARKitBlendShapePublisher,
 	public ILiveLinkSource
 {
 };
 
 /** Factory that creates and registers the sources with the LiveLink client */
-class APPLEARKIT_API FAppleARKitLiveLinkSourceFactory
+class APPLEARKITFACESUPPORT_API FAppleARKitLiveLinkSourceFactory
 {
 public:
 	/** Creates a face mesh source that will autobind to the tracked face mesh */
