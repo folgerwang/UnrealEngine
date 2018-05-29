1de4e70bae32f56b5e94490cdf249def3d7f8f35
Ben Marsh <Ben.Marsh@epicgames.com>
2018-05-29 14:45:36 -0400

    Merging latest fixes for UnrealGameSync from //UE4/Dev-Core.
    #rb none
    #rnx
    #jira
    
    [CL 4097818 by Ben Marsh in 4.20 branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSync/PerforceModalTask.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSync/PerforceModalTask.cs
@@ -45,7 +45,14 @@ namespace UnrealGameSync
 					PerforceConnection Perforce = new PerforceConnection(UserName, null, null);
 					if(ProjectFileName != null)
 					{
-						Directory.SetCurrentDirectory(Path.GetDirectoryName(ProjectFileName));
+						try
+						{
+							Directory.SetCurrentDirectory(Path.GetDirectoryName(ProjectFileName));
+						}
+						catch
+						{
+							// Just ignore
+						}
 					}
 
 					string NewServerAndPort;
