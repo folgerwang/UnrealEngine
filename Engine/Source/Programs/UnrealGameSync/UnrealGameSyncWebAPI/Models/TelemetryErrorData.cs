6ea00e46668778766fe0e1f356e9592d470e2e13
Ben Marsh <Ben.Marsh@epicgames.com>
2018-05-29 14:29:50 -0400

    Add missing copyright notices.
    #rb none
    #jira UE-59151
    #rnx
    
    [CL 4097759 by Ben Marsh in 4.20 branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSyncWebAPI/Models/TelemetryErrorData.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSyncWebAPI/Models/TelemetryErrorData.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace UnrealGameSyncWebAPI.Models
 {
-	
-
 	public class TelemetryErrorData
 	{
 		public enum TelemetryErrorType
