0eeb63b74fc068e818f340202106e9deddc37fbd
ben marsh <Ben.Marsh@epicgames.com>
2018-06-11 13:15:27 -0400

    #ROBOMERGE-AUTHOR: ben.marsh UBT: Fix issue causing incorrect compile environment for *.cc files. Treat them the same as .cpp files.
    #rb none
    #jira
    
    #ROBOMERGE-SOURCE: CL 4120474 in //UE4/Release-4.20/...
    #ROBOMERGE-BOT: RELEASE (Release-4.20 -> Release-Staging-4.20)
    
    [CL 4122754 by ben marsh in Staging-4.20 branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModuleCPP.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildModuleCPP.cs
@@ -655,19 +655,31 @@ namespace UnrealBuildTool
 			}
 
 			// Compile C files directly. Do not use a PCH here, because a C++ PCH is not compatible with C source files.
-			LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(ModuleCompileEnvironment, SourceFilesToBuild.CFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			if(SourceFilesToBuild.CFiles.Count > 0)
+			{
+				LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(ModuleCompileEnvironment, SourceFilesToBuild.CFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			}
 
 			// Compile CC files directly.
-			LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(CompileEnvironment, SourceFilesToBuild.CCFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			if(SourceFilesToBuild.CCFiles.Count > 0)
+			{
+				LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(CompileEnvironment, SourceFilesToBuild.CCFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			}
 
 			// Compile MM files directly.
-			LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(CompileEnvironment, SourceFilesToBuild.MMFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			if(SourceFilesToBuild.MMFiles.Count > 0)
+			{
+				LinkInputFiles.AddRange(ToolChain.CompileCPPFiles(CompileEnvironment, SourceFilesToBuild.MMFiles, IntermediateDirectory, Name, ActionGraph).ObjectFiles);
+			}
 
 			// Compile RC files. The resource compiler does not work with response files, and using the regular compile environment can easily result in the 
 			// command line length exceeding the OS limit. Use the binary compile environment to keep the size down, and require that all include paths
 			// must be specified relative to the resource file itself or Engine/Source.
-			CppCompileEnvironment ResourceCompileEnvironment = new CppCompileEnvironment(BinaryCompileEnvironment);
-			LinkInputFiles.AddRange(ToolChain.CompileRCFiles(ResourceCompileEnvironment, SourceFilesToBuild.RCFiles, IntermediateDirectory, ActionGraph).ObjectFiles);
+			if(SourceFilesToBuild.RCFiles.Count > 0)
+			{
+				CppCompileEnvironment ResourceCompileEnvironment = new CppCompileEnvironment(BinaryCompileEnvironment);
+				LinkInputFiles.AddRange(ToolChain.CompileRCFiles(ResourceCompileEnvironment, SourceFilesToBuild.RCFiles, IntermediateDirectory, ActionGraph).ObjectFiles);
+			}
 
 			// Write the compiled manifest
 			if(Rules.bPrecompile)
