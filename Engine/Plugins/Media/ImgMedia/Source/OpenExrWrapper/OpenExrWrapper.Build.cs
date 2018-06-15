c7a67f060cfeb1a9d4938c49d1c6777319a19f2d
brandon schaefer <Brandon.Schaefer@epicgames.com>
2018-06-15 14:37:47 -0400

    Enable Linux support for EXR
    #jira UE-60146
    #rb Arciel.Rekman
    
    #ROBOMERGE-SOURCE: CL 4133197 in //UE4/Release-4.20/...
    #ROBOMERGE-BOT: RELEASE (Release-4.20 -> Release-Staging-4.20)
    #ROBOMERGE-AUTHOR: brandon.schaefer
    
    [CL 4133199 by brandon schaefer in Staging-4.20 branch]

--- Engine/Plugins/Media/ImgMedia/Source/OpenExrWrapper/OpenExrWrapper.Build.cs
+++ Engine/Plugins/Media/ImgMedia/Source/OpenExrWrapper/OpenExrWrapper.Build.cs
@@ -19,6 +19,7 @@ namespace UnrealBuildTool.Rules
 
             if ((Target.Platform == UnrealTargetPlatform.Win64) ||
                 (Target.Platform == UnrealTargetPlatform.Win32) ||
+                (Target.Platform == UnrealTargetPlatform.Linux) ||
                 (Target.Platform == UnrealTargetPlatform.Mac))
             {
                 AddEngineThirdPartyPrivateStaticDependencies(Target, "UEOpenExr");
