94fbffb0b2d497a2e011a1318c31ad3a9e034796
Richard TalbotWatkin <Richard.TalbotWatkins@epicgames.com>
2018-06-18 11:26:58 -0400

    Copied from Dev-Editor CL 4118758
    - Refactor to put UStaticMesh Mesh Descriptions in a separate object which is not loaded by default, but which can be requested when needed. This needs to be kept in sync with the number of SourceModel LODs.
    - Various refactors to import/building.
    - Changed UMeshDescription to FMeshDescription, and made its preferred semantics pass-by-reference rather than by pointer.
    - Deprecated UMeshDescription.
    #rb Alexis.Matte
    #jira
    
    [CL 4135306 by Richard TalbotWatkin in 4.20 branch]

--- Engine/Source/Editor/UnrealEd/Private/Fbx/FbxFactory.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Fbx/FbxFactory.cpp
@@ -790,7 +790,7 @@ UObject* UFbxFactory::RecursiveImportNode(void* VoidFbxImporter, void* VoidNode,
 						//Add a Lod generated model
 						while (NewStaticMesh->SourceModels.Num() <= LODIndex)
 						{
-							new(NewStaticMesh->SourceModels) FStaticMeshSourceModel();
+							NewStaticMesh->AddSourceModel();
 						}
 						if (LODIndex - 1 > 0 && (NewStaticMesh->SourceModels[LODIndex - 1].ReductionSettings.PercentTriangles < 1.0f || NewStaticMesh->SourceModels[LODIndex - 1].ReductionSettings.MaxDeviation > 0.0f))
 						{
