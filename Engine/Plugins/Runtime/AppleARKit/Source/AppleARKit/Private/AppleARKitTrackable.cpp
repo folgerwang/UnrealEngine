4eeab204b057e181e76b9c80fe32efb1178611ae
Joe Graf <Joe.Graf@epicgames.com>
2018-06-20 18:35:44 -0400

    Merging using Dev-VR_to_Release-4.20
    #rb: WingnutAR
    #jira: UE-60884
    
    [CL 4151477 by Joe Graf in 4.20 branch]

--- Engine/Plugins/Runtime/AppleARKit/Source/AppleARKit/Private/AppleARKitTrackable.cpp
+++ Engine/Plugins/Runtime/AppleARKit/Source/AppleARKit/Private/AppleARKitTrackable.cpp
@@ -7,12 +7,14 @@ UAppleARKitEnvironmentCaptureProbe::UAppleARKitEnvironmentCaptureProbe()
 	: Super()
 	, ARKitEnvironmentTexture(nullptr)
 {
+#if SUPPORTS_ARKIT_2_0
 	ARKitEnvironmentTexture = NewObject<UAppleARKitEnvironmentCaptureProbeTexture>();
 	// Set the base class member since that's what gets used by the non-ARKit specific code
 	EnvironmentCaptureTexture = ARKitEnvironmentTexture;
+#endif
 }
 
-#if PLATFORM_MAC || PLATFORM_IOS
+#if SUPPORTS_ARKIT_2_0
 void UAppleARKitEnvironmentCaptureProbe::UpdateEnvironmentCapture(const TSharedRef<FARSystemBase, ESPMode::ThreadSafe>& InTrackingSystem, uint32 FrameNumber, double InTimestamp, const FTransform& InLocalToTrackingTransform, const FTransform& InAlignmentTransform, FVector InExtent, id<MTLTexture> InMetalTexture)
 {
 	Super::UpdateEnvironmentCapture(InTrackingSystem, FrameNumber, InTimestamp, InLocalToTrackingTransform, InAlignmentTransform, InExtent);
