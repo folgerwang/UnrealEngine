71c2d92a15ebce32e62dcb758b566562879e595c
Mark Satterthwaite <Mark.Satterthwaite@epicgames.com>
2018-06-22 18:33:37 -0400

    Fix buffer zero-initialisation and buffer-scribbling debug options in MetalRHI to try and help work out what's wrong on Intel. The FillBuffer function needs to be async. to avoid an issue with order of events when flush resource is involved. #rb none #jira UE-60328
    [CL 4156728 by Mark Satterthwaite in 4.20 branch]

--- Engine/Source/Runtime/Apple/MetalRHI/Private/MetalBuffer.cpp
+++ Engine/Source/Runtime/Apple/MetalRHI/Private/MetalBuffer.cpp
@@ -1178,6 +1178,27 @@ FMetalBuffer FMetalResourceHeap::CreateBuffer(uint32 Size, uint32 Alignment, mtl
 #endif
 	}
 	
+	if (GMetalBufferZeroFill)
+	{
+		switch(Buffer.GetStorageMode())
+		{
+			case mtlpp::StorageMode::Private:
+			{
+				mtlpp::CommandBuffer CmdBuffer = Queue->CreateCommandBuffer();
+				mtlpp::BlitCommandEncoder Encoder = CmdBuffer.BlitCommandEncoder();
+				Encoder.Fill(Buffer, ns::Range(0, Buffer.GetLength()), 0);
+				Encoder.EndEncoding();
+				Queue->CommitCommandBuffer(CmdBuffer);
+				break;
+			}
+			default:
+			{
+				FMemory::Memset(((uint8*)Buffer.GetContents()), 0, Buffer.GetLength());
+				break;
+			}
+		}
+	}
+	
     METAL_DEBUG_OPTION(GetMetalDeviceContext().ValidateIsInactiveBuffer(Buffer));
 	check(Buffer && Buffer.GetPtr());
 	return Buffer;
