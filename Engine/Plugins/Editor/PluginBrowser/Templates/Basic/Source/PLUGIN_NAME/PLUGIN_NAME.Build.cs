b442fa6c411c0e4fa6a03c90d15ad42cb3557dd2
ben marsh <Ben.Marsh@epicgames.com>
2018-06-27 16:14:19 -0400

    Remove incorrect include paths from default plugin configuration.
    #rb none
    #jira UE-60969
    
    #ROBOMERGE-SOURCE: CL 4160105 in //UE4/Release-4.20/...
    #ROBOMERGE-BOT: RELEASE (Release-4.20 -> Release-Staging-4.20)
    #ROBOMERGE-AUTHOR: ben.marsh
    
    [CL 4167199 by ben marsh in Staging-4.20 branch]

--- Engine/Plugins/Editor/PluginBrowser/Templates/Basic/Source/PLUGIN_NAME/PLUGIN_NAME.Build.cs
+++ Engine/Plugins/Editor/PluginBrowser/Templates/Basic/Source/PLUGIN_NAME/PLUGIN_NAME.Build.cs
@@ -10,7 +10,6 @@ public class PLUGIN_NAME : ModuleRules
 		
 		PublicIncludePaths.AddRange(
 			new string[] {
-				"PLUGIN_NAME/Public"
 				// ... add public include paths required here ...
 			}
 			);
@@ -18,7 +17,6 @@ public class PLUGIN_NAME : ModuleRules
 		
 		PrivateIncludePaths.AddRange(
 			new string[] {
-				"PLUGIN_NAME/Private",
 				// ... add other private include paths required here ...
 			}
 			);
