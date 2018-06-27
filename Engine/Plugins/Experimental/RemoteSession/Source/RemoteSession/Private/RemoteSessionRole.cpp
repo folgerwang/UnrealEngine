decb8e304dade2648425c6b923eb42058423ea8e
joe graf <Joe.Graf@epicgames.com>
2018-06-27 16:58:23 -0400

    Merging host fix for bSupportAR using Dev-VR_to_Release-4.20
    #rb: merge
    #jira: UE-61148
    
    #ROBOMERGE-SOURCE: CL 4162327 in //UE4/Release-4.20/...
    #ROBOMERGE-BOT: RELEASE (Release-4.20 -> Release-Staging-4.20)
    #ROBOMERGE-AUTHOR: joe.graf
    
    [CL 4167512 by joe graf in Staging-4.20 branch]

--- Engine/Plugins/Experimental/RemoteSession/Source/RemoteSession/Private/RemoteSessionRole.cpp
+++ Engine/Plugins/Experimental/RemoteSession/Source/RemoteSession/Private/RemoteSessionRole.cpp
@@ -212,7 +212,9 @@ void FRemoteSessionRole::CreateChannel(const FString& InChannelName, ERemoteSess
     else if (InChannelName == FRemoteSessionXRTrackingChannel::StaticType())
 	{
 		// Skip if there isn't an AR session running
-		if (UARBlueprintLibrary::GetARSessionStatus().Status == EARSessionStatus::Running)
+		if (UARBlueprintLibrary::GetARSessionStatus().Status == EARSessionStatus::Running ||
+			// Add our listener if we are the host
+			InMode == ERemoteSessionChannelMode::Read)
 		{
 			NewChannel = MakeShareable(new FRemoteSessionXRTrackingChannel(InMode, OSCConnection));
 		}
@@ -224,7 +226,9 @@ void FRemoteSessionRole::CreateChannel(const FString& InChannelName, ERemoteSess
 	else if (InChannelName == FRemoteSessionARCameraChannel::StaticType())
 	{
 		// Skip if there isn't an AR session running
-		if (UARBlueprintLibrary::GetARSessionStatus().Status == EARSessionStatus::Running)
+		if (UARBlueprintLibrary::GetARSessionStatus().Status == EARSessionStatus::Running ||
+			// Add our listener if we are the host
+			InMode == ERemoteSessionChannelMode::Read )
 		{
 			// Client side sending only works on iOS with Android coming in the future
 			bool IsSupported = (InMode == ERemoteSessionChannelMode::Read) || PLATFORM_IOS;
