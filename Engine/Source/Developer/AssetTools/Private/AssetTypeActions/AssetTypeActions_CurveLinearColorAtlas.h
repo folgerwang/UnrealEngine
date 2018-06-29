4cce4e850b1612f60df015edc91a606885c6bf46
lauren ridge <Lauren.Ridge@epicgames.com>
2018-06-29 11:53:09 -0400

    Moving curve atlas filter to match asset placement in CB #jira UE-61329 #rb trivial #rnx
    #ROBOMERGE-SOURCE: CL 4173939 in //UE4/Release-4.20/...
    #ROBOMERGE-BOT: RELEASE (Release-4.20 -> Release-Staging-4.20)
    #ROBOMERGE-AUTHOR: lauren.ridge
    
    [CL 4173942 by lauren ridge in Staging-4.20 branch]

--- Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_CurveLinearColorAtlas.h
+++ Engine/Source/Developer/AssetTools/Private/AssetTypeActions/AssetTypeActions_CurveLinearColorAtlas.h
@@ -19,4 +19,5 @@ public:
 	virtual void GetActions( const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder ) override;
 	virtual bool IsImportedAsset() const override { return false; }
 	virtual bool CanFilter() override { return true; }
+	virtual uint32 GetCategories() override { return EAssetTypeCategories::Misc; }
 };
