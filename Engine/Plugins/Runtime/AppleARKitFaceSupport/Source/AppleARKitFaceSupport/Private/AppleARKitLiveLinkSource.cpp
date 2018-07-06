51c7b16689a211c02b11fe42eeb2266fe7355855
Joe Graf <Joe.Graf@epicgames.com>
2018-07-06 11:04:32 -0400

    Work around LiveLink interpreting ARKit timestamps incorrectly causing jitter and animation lag
    #rb: david.hibbitts
    #jira: UE-61540
    
    [CL 4188782 by Joe Graf in Dev-VR branch]

--- Engine/Plugins/Runtime/AppleARKitFaceSupport/Source/AppleARKitFaceSupport/Private/AppleARKitLiveLinkSource.cpp
+++ Engine/Plugins/Runtime/AppleARKitFaceSupport/Source/AppleARKitFaceSupport/Private/AppleARKitLiveLinkSource.cpp
@@ -147,8 +147,10 @@ void FAppleARKitLiveLinkSource::PublishBlendShapes(FName SubjectName, double Tim
 	{
 		static FLiveLinkFrameData LiveLinkFrame;
 
-		LiveLinkFrame.WorldTime = Timestamp;
-
+		//LiveLinkFrame.WorldTime = Timestamp;
+		// Don't use the ARKit timestamps because there's a bug somewhere in LiveLink that doesn't interpret them properly
+		LiveLinkFrame.WorldTime = FPlatformTime::Seconds();
+		
 		TArray<FLiveLinkCurveElement>& BlendShapes = LiveLinkFrame.CurveElements;
 
 		BlendShapes.Reset((int32)EARFaceBlendShape::MAX);
