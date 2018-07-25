527cbc06acb96db2797a672e1083a9c1a5a5f13a
Keli Hlodversson <Keli.Hlodversson@epicgames.com>
2018-07-25 08:15:07 -0400

    Updating to SteamVR 1.0.15 #rb SDKUpdate
    [CL 4230863 by Keli Hlodversson in Dev-VR branch]

--- Engine/Source/ThirdParty/OpenVR/OpenVR.build.cs
+++ Engine/Source/ThirdParty/OpenVR/OpenVR.build.cs
@@ -9,7 +9,7 @@ public class OpenVR : ModuleRules
 	public OpenVR(ReadOnlyTargetRules Target) : base(Target)
 	{
 		/** Mark the current version of the OpenVR SDK */
-		string OpenVRVersion = "v1_0_11";
+		string OpenVRVersion = "v1_0_15";
 		Type = ModuleType.External;
 
 		string SdkBase = Target.UEThirdPartySourceDirectory + "OpenVR/OpenVR" + OpenVRVersion;
