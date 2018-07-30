aee71dd9166ae106cf26e75e56c66136387ac625
Jamie Dale <Jamie.Dale@epicgames.com>
2018-07-30 15:05:21 -0400

    Made the NoExport FDateTime and FTimespan struct Tick data actual properties
    #rb Francis.Hurteau
    
    [CL 4241563 by Jamie Dale in Dev-Editor branch]

--- Engine/Source/Runtime/CoreUObject/Public/UObject/NoExportTypes.h
+++ Engine/Source/Runtime/CoreUObject/Public/UObject/NoExportTypes.h
@@ -944,6 +944,7 @@ public:
 USTRUCT(immutable, noexport, BlueprintType, meta=(HasNativeMake="Engine.KismetMathLibrary.MakeDateTime", HasNativeBreak="Engine.KismetMathLibrary.BreakDateTime"))
 struct FDateTime
 {
+	UPROPERTY()
 	int64 Ticks;
 };
 
@@ -1021,6 +1022,7 @@ struct FTimecode
 USTRUCT(immutable, noexport, BlueprintType, meta=(HasNativeMake="Engine.KismetMathLibrary.MakeTimespan", HasNativeBreak="Engine.KismetMathLibrary.BreakTimespan"))
 struct FTimespan
 {
+	UPROPERTY()
 	int64 Ticks;
 };
 
