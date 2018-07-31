de81cdfe8fc6a2f0a678aaa73de9b0ffbe39393c
Nick Shin <nshin@phosphorgames.com>
2018-07-31 17:13:51 -0400

    HTML5 - syncing build scripts
    #jria UEMOB-255  HTML5 Multithreading
    #rn
    #rb none
    
    [CL 4244686 by Nick Shin in Dev-Mobile branch]

--- Engine/Source/ThirdParty/SDL2/build_html5.sh
+++ Engine/Source/ThirdParty/SDL2/build_html5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-OGG_HTML5=$(pwd)
+SDL2_HTML5=$(pwd)
 
-cd ../../../../HTML5/
+cd ../HTML5/
 	. ./Build_All_HTML5_libs.rc
-cd "$OGG_HTML5"
+cd "$SDL2_HTML5/SDL-gui-backend"
 
 
 build_via_cmake()
@@ -27,48 +27,42 @@ build_via_cmake()
 #	EMFLAGS="-msse2 -s SIMD=0 -s USE_PTHREADS=1 -s WASM=1 -s BINARYEN=1" # WASM still does not play nice with SIMD
 	EMFLAGS="-s SIMD=0 -s USE_PTHREADS=1"
 	# ----------------------------------------
-	EMPATH=$(dirname `which emcc.py`)
-#	EXTRAFLAGS="-isystem$EMPATH/system/include/libc" # 1.37.36 needs this...
-	export CFLAGS="-I$EMPATH/system/include/libc" # 1.37.36 needs this...
-	# ----------------------------------------
 	emcmake cmake -G "Unix Makefiles" \
-		-DBUILD_SHARED_LIBS=OFF \
+		-DBUILD_SHARED_LIBS=FALSE \
+		-DLIBTYPE=STATIC \
 		-DEMSCRIPTEN_GENERATE_BITCODE_STATIC_LIBRARIES=ON \
 		-DCMAKE_BUILD_TYPE=$type \
 		-DCMAKE_C_FLAGS_$TYPE="$OPTIMIZATION -D$DBGFLAG $EMFLAGS" \
-		../../..
-#	cmake -LAH
-#	exit
+		..
 	cmake --build . -- -j VERBOSE=1 2>&1 | tee zzz_build.log
 	# ----------------------------------------
 	if [ $OLEVEL == 0 ]; then
 		SUFFIX=
 	fi
-	cp libogg.bc ../../../lib/HTML5/libogg${SUFFIX}.bc
+	cp libSDL2.bc ../lib/HTML5/libSDL2${SUFFIX}.bc
 	cd ..
 }
+chmod +w lib/HTML5/*
 type=Debug;       OLEVEL=0;  build_via_cmake
 type=Release;     OLEVEL=2;  build_via_cmake
 type=Release;     OLEVEL=3;  build_via_cmake
 type=MinSizeRel;  OLEVEL=z;  build_via_cmake
-ls -l ../../lib/HTML5
+ls -l lib/HTML5
 
+
+exit
 
 # NOT USED: LEFT HERE FOR REFERENCE
+# NOT USED: LEFT HERE FOR REFERENCE
+# NOT USED: LEFT HERE FOR REFERENCE
+
+
 build_via_makefile()
 {
-	cd ../../src
+	cd ../../Src
 	
-	if [ ! -e "../include/ogg/config_types.h" ]; then
-		# WARNING: on first time, the following must be done:
-		cd ..
-		chmod 755 configure
-		emconfigure ./configure
-		cd src
-	fi
-	
-	proj=libogg
-	makefile=../build/HTML5/Makefile.HTML5
+	proj=libSDL2
+	makefile=../Builds/html5/Makefile.HTML5
 	EMFLAGS="-msse -msse2 -s FULL_ES2=1 -s USE_PTHREADS=1"
 	
 	make clean   OPTIMIZATION=-O3 CFLAGS_EXTRA="$EMFLAGS" LIB=${proj}_O3.bc -f ${makefile}
@@ -83,7 +77,7 @@ build_via_makefile()
 	make clean   OPTIMIZATION=-O0 CFLAGS_EXTRA="$EMFLAGS" LIB=${proj}.bc -f ${makefile}
 	make install OPTIMIZATION=-O0 CFLAGS_EXTRA="$EMFLAGS" LIB=${proj}.bc -f ${makefile}
 	
-	ls -l ../lib/HTML5
+	ls -l ../Lib/HTML5
 }
 
 # no longer needed with latest emscripten:
