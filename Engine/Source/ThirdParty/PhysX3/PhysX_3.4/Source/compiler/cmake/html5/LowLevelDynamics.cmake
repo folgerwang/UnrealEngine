80f17212c638f8f4e3c57492d9b54df796d5f048
Nick Shin <nshin@phosphorgames.com>
2018-08-01 09:49:08 -0400

    HTML5 - syncing PhysX build scripts
    #jria UEMOB-255  HTML5 Multithreading
    #rn
    #rb none
    
    [CL 4246399 by Nick Shin in Dev-Mobile branch]

--- Engine/Source/ThirdParty/PhysX3/PhysX_3.4/Source/compiler/cmake/html5/LowLevelDynamics.cmake
+++ Engine/Source/ThirdParty/PhysX3/PhysX_3.4/Source/compiler/cmake/html5/LowLevelDynamics.cmake
@@ -6,14 +6,21 @@ SET(PHYSX_SOURCE_DIR ${PROJECT_SOURCE_DIR}/../../../)
 
 SET(LL_SOURCE_DIR ${PHYSX_SOURCE_DIR}/LowLevelDynamics/src)
 
+FIND_PACKAGE(nvToolsExt REQUIRED)
+
 SET(LOWLEVELDYNAMICS_PLATFORM_INCLUDES
 	$ENV{EMSCRIPTEN}/system/include
+	${NVTOOLSEXT_INCLUDE_DIRS}
+	${PHYSX_SOURCE_DIR}/Common/src/linux
+	${PHYSX_SOURCE_DIR}/LowLevel/software/include/linux
+	${PHYSX_SOURCE_DIR}/LowLevelDynamics/include/linux
+	${PHYSX_SOURCE_DIR}/LowLevel/common/include/pipeline/linux
 )
 
 SET(LOWLEVELDYNAMICS_COMPILE_DEFS
 
 	# Common to all configurations
-	${PHYSX_HTML5_COMPILE_DEFS};
+	${PHYSX_HTML5_COMPILE_DEFS};PX_PHYSX_STATIC_LIB;
 
 	$<$<CONFIG:debug>:${PHYSX_HTML5_DEBUG_COMPILE_DEFS};>
 	$<$<CONFIG:checked>:${PHYSX_HTML5_CHECKED_COMPILE_DEFS};>
