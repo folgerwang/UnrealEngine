7450ddbe46c36a1dffae13b486002095eed2e76c
Martin Wilson <Martin.Wilson@epicgames.com>
2018-08-13 06:57:16 -0400

    Native notifies for dynamics and cloth management
    #jira UE-55162
    #rb Benn.Gallagher
    
    [CL 4278903 by Martin Wilson in Dev-Anim branch]

---
+++ Engine/Source/Runtime/Engine/Classes/Animation/AnimNotifies/AnimNotify_ResumeClothingSimulation.h
@@ -0,0 +1,26 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "AnimNotify.h"
+
+#include "AnimNotify_ResumeClothingSimulation.generated.h"
+
+UCLASS(const, hidecategories=Object, collapsecategories, meta=(DisplayName="Resume Clothing Simulation"))
+class ENGINE_API UAnimNotify_ResumeClothingSimulation : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+
+	UAnimNotify_ResumeClothingSimulation();
+
+	// Begin UAnimNotify interface
+	virtual FString GetNotifyName_Implementation() const override;
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+	// End UAnimNotify interface
+
+};
+
+
+
