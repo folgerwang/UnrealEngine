ae37031ca139cf1bfadbfe346093a783d969b8b0
mark satterthwaite <Mark.Satterthwaite@epicgames.com>
2018-09-28 11:43:08 -0400

    Duplicate CL #4399472: Handle all Metal formats in ShaderPlatformToPlatformName - I have not made this handle all formats or platforms so this function is *wrong* for a number of others and should *assert* on failure to identify a platform. This fixes incorrect shader compilation for Forward rendering on iOS.
    #rb none
    #jira UE-64355
    [FYI] uriel.doyon
    
    #ROBOMERGE-SOURCE: CL 4405733 in //UE4/Release-4.21/...
    #ROBOMERGE-BOT: RELEASE (Release-4.21 -> Release-Staging-4.21)
    
    [CL 4405736 by mark satterthwaite in Staging-4.21 branch]

--- Engine/Source/Runtime/RHI/Private/RHI.cpp
+++ Engine/Source/Runtime/RHI/Private/RHI.cpp
@@ -686,8 +686,13 @@ FName ShaderPlatformToPlatformName(EShaderPlatform Platform)
 	case SP_VULKAN_ES3_1_ANDROID:
 		return NAME_PLATFORM_ANDROID;
 	case SP_METAL:
+	case SP_METAL_MRT:
 		return NAME_PLATFORM_IOS;
 	case SP_METAL_SM5:
+	case SP_METAL_SM5_NOTESS:
+	case SP_METAL_MACES3_1:
+	case SP_METAL_MACES2:
+	case SP_METAL_MRT_MAC:
 		return NAME_PLATFORM_MAC;
 	case SP_SWITCH:
 	case SP_SWITCH_FORWARD:
