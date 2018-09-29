71c87dc4b6b16bd8c324b17bbdaa092eff17800b
Max Chen <Max.Chen@epicgames.com>
2018-09-29 02:28:37 -0400

    Sequencer: Removed derived blueprint type for level sequence director blueprints
    #jira UE-64258
    #rb andrew.rodham
    
    [CL 4408662 by Max Chen in 4.21 branch]

--- Engine/Source/Runtime/LevelSequence/Private/LevelSequenceDirector.cpp
+++ Engine/Source/Runtime/LevelSequence/Private/LevelSequenceDirector.cpp
@@ -1,7 +1,7 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
-#include "LevelSequenceDirectorGeneratedClass.h"
-#include "LevelSequencePlayer.h"
+#include "LevelSequenceDirector.h"
+#include "Engine/World.h"
 
 UWorld* ULevelSequenceDirector::GetWorld() const
 {
