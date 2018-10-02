d4dad74728d8c38815eb17c748bbea4b7e93279b
ben marsh <Ben.Marsh@epicgames.com>
2018-10-02 10:05:24 -0400

    Include portable callstacks in ensures.
    #rb none
    #jira UE-64574
    
    #ROBOMERGE-SOURCE: CL 4414930 in //UE4/Release-4.21/...
    #ROBOMERGE-BOT: RELEASE (Release-4.21 -> Release-Staging-4.21)
    
    [CL 4414931 by ben marsh in Staging-4.21 branch]

--- Engine/Source/Runtime/Core/Private/Unix/UnixPlatformCrashContext.cpp
+++ Engine/Source/Runtime/Core/Private/Unix/UnixPlatformCrashContext.cpp
@@ -285,7 +285,7 @@ void FUnixCrashContext::CaptureStackTrace()
 		StackTrace[0] = 0;
 
 		int32 IgnoreCount = 0;
-		FGenericCrashContext::GeneratePortableCallStack(IgnoreCount, StackTraceSize, this);
+		CapturePortableCallStack(IgnoreCount, this);
 
 		// Walk the stack and dump it to the allocated memory (do not ignore any stack frames to be consistent with check()/ensure() handling)
 		FPlatformStackWalk::StackWalkAndDump( StackTrace, StackTraceSize, IgnoreCount, this);
