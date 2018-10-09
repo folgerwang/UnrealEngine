d35de57866fac3995c6a07802546f8c3fbc37e09
dan oconnor <Dan.OConnor@epicgames.com>
2018-10-09 19:23:06 -0400

    Fixed crash after hitting stop when in-stack-debugging Clear GPlayInEditorID when stopping PIE at a blueprint exception/breakpoint #rb Ben.Zeigler #jira UE-64895, UE-65005, UE-65008
    #ROBOMERGE-SOURCE: CL 4443217 in //UE4/Release-4.21/...
    #ROBOMERGE-BOT: RELEASE (Release-4.21 -> Release-Staging-4.21)
    
    [CL 4443218 by dan oconnor in Staging-4.21 branch]

--- Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetDebugUtilities.cpp
+++ Engine/Source/Editor/UnrealEd/Private/Kismet2/KismetDebugUtilities.cpp
@@ -702,6 +702,7 @@ void FKismetDebugUtilities::AttemptToBreakExecution(UBlueprint* BlueprintObj, co
 	// Now enter within-the-frame debugging mode
 	if (bShouldInStackDebug)
 	{
+		TGuardValue<int32> GuardDisablePIE(GPlayInEditorID, INDEX_NONE);
 		const TArray<const FFrame*>& ScriptStack = FBlueprintExceptionTracker::Get().ScriptStack;
 		Data.LastExceptionMessage = Info.GetDescription();
 		FKismetEditorUtilities::BringKismetToFocusAttentionOnObject(NodeStoppedAt);
