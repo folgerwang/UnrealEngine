64c121b0d55db3ad07a38cf0e1b61de16dfce92c
Chris Babcock <Chris.Babcock@epicgames.com>
2018-10-12 14:45:52 -0400

    EnableSnapshot for Google Play Games login (contributed by rainlabs)
    #jira UE-60685
    #PR #4805
    #ue4
    #android
    #rb trivial
    #lockdown Jack.Porter
    
    [CL 4461305 by Chris Babcock in Dev-Mobile branch]

--- Engine/Plugins/Online/Android/OnlineSubsystemGooglePlay/Source/Private/OnlineAsyncTaskGooglePlayLogin.cpp
+++ Engine/Plugins/Online/Android/OnlineSubsystemGooglePlay/Source/Private/OnlineAsyncTaskGooglePlayLogin.cpp
@@ -42,6 +42,7 @@ void FOnlineAsyncTaskGooglePlayLogin::Start_OnTaskThread()
 					*FString(DebugString(LocalStatus).c_str()));
 				LocalSubsystem->OnAuthActionFinished(Op, LocalStatus);
 			})
+			.EnableSnapshots()
 			.AddOauthScope("https://www.googleapis.com/auth/plus.login")
 			.Create(Subsystem->PlatformConfiguration);
 	}
