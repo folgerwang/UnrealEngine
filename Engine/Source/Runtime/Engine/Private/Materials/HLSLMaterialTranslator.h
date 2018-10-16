37534a4d6b15b41fcbf715c38172983af6ae3339
dmitriy dyomin <Dmitriy.Dyomin@epicgames.com>
2018-10-15 22:42:49 -0400

    Fixed: PostProcessing materails that sample SceneDepth fail when MSAA is enabled on Mobile SceneDepth can only be sampled in PP materails that have blendable location set to BeforeTranslucency or BeforeTonemapping #jira UE-64245 #rb none
    #ROBOMERGE-SOURCE: CL 4472090 in //UE4/Release-4.21/...
    #ROBOMERGE-BOT: RELEASE (Release-4.21 -> Release-Staging-4.21)
    
    [CL 4472091 by dmitriy dyomin in Staging-4.21 branch]

--- Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.h
+++ Engine/Source/Runtime/Engine/Private/Materials/HLSLMaterialTranslator.h
@@ -3787,6 +3787,14 @@ protected:
 			// On mobile in post process material, there is no need to do ViewportUV->BufferUV conversion because ViewSize == BufferSize.
 			if (Material->GetMaterialDomain() == MD_PostProcess)
 			{
+				int32 BlendableLocation = Material->GetBlendableLocation();
+				if (SceneTextureId == PPI_SceneDepth && !(BlendableLocation == BL_BeforeTranslucency || BlendableLocation == BL_BeforeTonemapping))
+				{
+					// SceneDepth lookups are not available when using MSAA, but we can access depth stored in SceneColor.A channel
+					// SceneColor.A channel holds depth till BeforeTonemapping location, then it's gets overwritten
+					return Errorf(TEXT("SceneDepth lookups are only available when BlendableLocation is BeforeTranslucency or BeforeTonemapping"));
+				}
+				
 				if (ViewportUV == INDEX_NONE)
 				{
 					UV = TextureCoordinate(0, false, false);
