c1820e607e7bad4fedff63f64e0cd0cac542951d
lauren ridge <Lauren.Ridge@epicgames.com>
2018-10-16 17:32:33 -0400

    Fix for default asset names #jira UE-65301 #rb trivial
    #ROBOMERGE-SOURCE: CL 4476539 in //UE4/Release-4.21/...
    #ROBOMERGE-BOT: RELEASE (Release-4.21 -> Release-Staging-4.21)
    
    [CL 4476544 by lauren ridge in Staging-4.21 branch]

--- Engine/Source/Editor/UnrealEd/Classes/Factories/MaterialFunctionMaterialLayerFactory.h
+++ Engine/Source/Editor/UnrealEd/Classes/Factories/MaterialFunctionMaterialLayerFactory.h
@@ -19,6 +19,10 @@ class UMaterialFunctionMaterialLayerFactory : public UFactory
 	//~ Begin UFactory Interface
 	virtual bool CanCreateNew() const override;
 	virtual UObject* FactoryCreateNew(UClass* Class,UObject* InParent,FName Name,EObjectFlags Flags,UObject* Context,FFeedbackContext* Warn) override;
+	FString GetDefaultNewAssetName() const override
+	{
+		return FString(TEXT("NewMaterialLayer"));
+	}
 	//~ Begin UFactory Interface	
 };
 
