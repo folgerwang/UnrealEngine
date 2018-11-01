28ffdd28ebaaf2c2de0d9e1dd8396ca784927d40
Jack Porter <jack.porter@epicgames.com>
2018-11-01 02:46:15 -0400

    Fixed issue where new Mobile PIE doesn't remember size setting from previous session #jira UE-64591 #rb Chris.Babcock #fyi Bogdan.Vasilache
    [CL 4530394 by Jack Porter in 4.21 branch]

---
+++ Engine/Source/Editor/PIEPreviewDeviceProfileSelector/Private/PIEPreviewSettings.h
@@ -0,0 +1,29 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+/**
+*
+* A configuration class used by the PIE preview device system to save settings across sessions.
+*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/Object.h"
+#include "PIEPreviewSettings.generated.h"
+
+UCLASS(hidecategories = Object, config = PIEPreviewSettings)
+class PIEPREVIEWDEVICEPROFILESELECTOR_API UPIEPreviewSettings : public UObject
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+	UPROPERTY(config)
+	int32 WindowPosX;
+
+	UPROPERTY(config)
+	int32 WindowPosY;
+
+	UPROPERTY(config)
+	float WindowScalingFactor;
+};
