9361422fa497b8e42a8031ac424494034951e221
Chris Gagnon <Chris.Gagnon@epicgames.com>
2018-11-12 11:14:14 -0500

    Adjusted padding slightly to ensure highliglting looks good.
    #jira UE-66377
    #codereview Lauren.Ridge
    #rb Lauren.Ridge
    
    [CL 4559131 by Chris Gagnon in Dev-Editor branch]

--- Engine/Source/Editor/PropertyEditor/Private/SDetailsView.cpp
+++ Engine/Source/Editor/PropertyEditor/Private/SDetailsView.cpp
@@ -197,14 +197,14 @@ void SDetailsView::Construct(const FArguments& InArgs)
 		[
 			SNew(SOverlay)
 			+SOverlay::Slot()
-			.Padding(2.0f, 0.0f, 0.0f, 0.0f)
+			.Padding(0.0f, 0.0f, 0.0f, 0.0f)
 			[
 				SNew(SImage)
 				.Image(FEditorStyle::GetBrush("Searching.SearchActiveTab"))
 				.Visibility_Lambda([&](){ return this->bHasActiveFilter ? EVisibility::Visible : EVisibility::Collapsed; })
 			]
 			+SOverlay::Slot()
-			.Padding(0.f, 2.0f, 4.f, 2.f)
+			.Padding(2.f, 2.0f, 4.f, 2.f)
 			.VAlign( VAlign_Center )
 			[
 				// Create the search box
