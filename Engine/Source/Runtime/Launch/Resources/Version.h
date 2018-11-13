90a7277e44eede0ae40bb5275cd676e5153ea2bb
Ben Marsh <Ben.Marsh@epicgames.com>
2018-11-12 23:38:44 -0500

    Update version to 4.21.1.
    #rb none
    #jira UE-66424
    
    [CL 4561084 by Ben Marsh in 4.21 branch]

--- Engine/Source/Runtime/Launch/Resources/Version.h
+++ Engine/Source/Runtime/Launch/Resources/Version.h
@@ -56,7 +56,7 @@
 // newer than a 4.11.* version, regardless of the changelist that it was built with)
 #define ENGINE_MAJOR_VERSION	4
 #define ENGINE_MINOR_VERSION	21
-#define ENGINE_PATCH_VERSION	0
+#define ENGINE_PATCH_VERSION	1
 
 // Macros for encoding strings
 #define VERSION_TEXT(x) TEXT(x)
