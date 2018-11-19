549191b7320c2af1e5b20e6d67f8b4699417ab74
Stuart Hill <Stuart.Hill@epicgames.com>
2018-11-19 12:16:44 -0500

    UE-66357 Pixel Streaming. Allow the Proxy connection to be able to handle the situation where it doesn't yet have an input device.
    #jira UE-66357
    #rb Alistair.White
    #lockdown Cristina.Riveron
    
    [CL 4578857 by Stuart Hill in 4.21 branch]

--- Engine/Plugins/Experimental/PixelStreaming/Source/PixelStreaming/Private/PixelStreamingPlugin.cpp
+++ Engine/Plugins/Experimental/PixelStreaming/Source/PixelStreaming/Private/PixelStreamingPlugin.cpp
@@ -1,8 +1,8 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
+#include "PixelStreamingPlugin.h"
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
-#include "IPixelStreamingPlugin.h"
 #include "UObject/UObjectIterator.h"
 #include "Engine/GameEngine.h"
 #include "Engine/GameViewportClient.h"
@@ -27,156 +27,150 @@ DEFINE_LOG_CATEGORY(PixelStreamingInput);
 DEFINE_LOG_CATEGORY(PixelStreamingNet);
 DEFINE_LOG_CATEGORY(PixelStreamingCapture);
 
-class FPixelStreamingPlugin : public IPixelStreamingPlugin
+/** IModuleInterface implementation */
+void FPixelStreamingPlugin::StartupModule()
 {
-public:
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override
-	{
-		// detect hardware capabilities, init nvidia capture libs, etc
+	// detect hardware capabilities, init nvidia capture libs, etc
 
-		// subscribe to engine delegates here for init / framebuffer creation / whatever
-		if (UGameEngine* GameEngine = Cast<UGameEngine>(GEngine))
-		{
-			if (FSlateApplication::IsInitialized())
-			{
-				FSlateRenderer::FOnBackBufferReadyToPresent OnBackBufferReadyDelegate;
-				OnBackBufferReadyDelegate.BindRaw(this, &FPixelStreamingPlugin::OnBackBufferReady_RenderThread);
-				FSlateApplication::Get().GetRenderer()->OnBackBufferReadyToPresent() = OnBackBufferReadyDelegate;
-
-				FSlateApplication::Get().GetRenderer()->OnPreResizeWindowBackBuffer().AddRaw(this, &FPixelStreamingPlugin::OnPreResizeWindowBackbuffer);
-			}
-
-		}
-
-		FGameModeEvents::GameModePostLoginEvent.AddRaw(this, &FPixelStreamingPlugin::OnGameModePostLogin);
-		FGameModeEvents::GameModeLogoutEvent.AddRaw(this, &FPixelStreamingPlugin::OnGameModeLogout);
-
-		IModularFeatures::Get().RegisterModularFeature(GetModularFeatureName(), this);
-
-		FApp::SetUnfocusedVolumeMultiplier(1.0f);
-	}
-
-	virtual void ShutdownModule() override
+	// subscribe to engine delegates here for init / framebuffer creation / whatever
+	if (UGameEngine* GameEngine = Cast<UGameEngine>(GEngine))
 	{
 		if (FSlateApplication::IsInitialized())
 		{
-			FSlateApplication::Get().GetRenderer()->OnBackBufferReadyToPresent().Unbind();
-			FSlateApplication::Get().GetRenderer()->OnPreResizeWindowBackBuffer().RemoveAll(this);
+			FSlateRenderer::FOnBackBufferReadyToPresent OnBackBufferReadyDelegate;
+			OnBackBufferReadyDelegate.BindRaw(this, &FPixelStreamingPlugin::OnBackBufferReady_RenderThread);
+			FSlateApplication::Get().GetRenderer()->OnBackBufferReadyToPresent() = OnBackBufferReadyDelegate;
+
+			FSlateApplication::Get().GetRenderer()->OnPreResizeWindowBackBuffer().AddRaw(this, &FPixelStreamingPlugin::OnPreResizeWindowBackbuffer);
 		}
 
-		IModularFeatures::Get().UnregisterModularFeature(GetModularFeatureName(), this);
 	}
 
-private:
-	void UpdateViewport(FSceneViewport* Viewport)
+	FGameModeEvents::GameModePostLoginEvent.AddRaw(this, &FPixelStreamingPlugin::OnGameModePostLogin);
+	FGameModeEvents::GameModeLogoutEvent.AddRaw(this, &FPixelStreamingPlugin::OnGameModeLogout);
+
+	IModularFeatures::Get().RegisterModularFeature(GetModularFeatureName(), this);
+
+	FApp::SetUnfocusedVolumeMultiplier(1.0f);
+}
+
+void FPixelStreamingPlugin::ShutdownModule()
+{
+	if (FSlateApplication::IsInitialized())
 	{
-		FRHIViewport* const ViewportRHI = Viewport->GetViewportRHI().GetReference();
+		FSlateApplication::Get().GetRenderer()->OnBackBufferReadyToPresent().Unbind();
+		FSlateApplication::Get().GetRenderer()->OnPreResizeWindowBackBuffer().RemoveAll(this);
 	}
 
-	void OnBackBufferReady_RenderThread(const FTexture2DRHIRef& BackBuffer)
-	{
-		check(IsInRenderingThread());
+	IModularFeatures::Get().UnregisterModularFeature(GetModularFeatureName(), this);
+}
 
-		if (!Streamer)
+void FPixelStreamingPlugin::UpdateViewport(FSceneViewport* Viewport)
+{
+	FRHIViewport* const ViewportRHI = Viewport->GetViewportRHI().GetReference();
+}
+
+void FPixelStreamingPlugin::OnBackBufferReady_RenderThread(const FTexture2DRHIRef& BackBuffer)
+{
+	check(IsInRenderingThread());
+
+	if (!Streamer)
+	{
+		FString IP = TEXT("0.0.0.0");
+		FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingIP="), IP);
+		uint16 Port = 8124;
+		FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingPort="), Port);
+
+		Streamer = MakeUnique<FStreamer>(*IP, Port, BackBuffer);
+	}
+
+	Streamer->OnFrameBufferReady(BackBuffer);
+}
+
+void FPixelStreamingPlugin::OnPreResizeWindowBackbuffer(void* BackBuffer)
+{
+	if (Streamer)
+	{
+		ENQUEUE_UNIQUE_RENDER_COMMAND_ONEPARAMETER(
+			FPixelStreamingOnPreResizeWindowBackbuffer,
+			FPixelStreamingPlugin*, Plugin, this,
 		{
-			FString IP = TEXT("0.0.0.0");
-			FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingIP="), IP);
-			uint16 Port = 8124;
-			FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingPort="), Port);
+			Plugin->OnPreResizeWindowBackbuffer_RenderThread();
+		});	
 
-			Streamer = MakeUnique<FStreamer>(*IP, Port, BackBuffer);
-		}
+		// Make sure OnPreResizeWindowBackbuffer_RenderThread is executed before continuing
+		FlushRenderingCommands();
+	}
+}
 
-		Streamer->OnFrameBufferReady(BackBuffer);
+void FPixelStreamingPlugin::OnPreResizeWindowBackbuffer_RenderThread()
+{
+	Streamer->OnPreResizeWindowBackbuffer();
+}
+
+TSharedPtr<class IInputDevice> FPixelStreamingPlugin::CreateInputDevice(const TSharedRef<FGenericApplicationMessageHandler>& InMessageHandler)
+{
+	InputDevice = MakeShareable(new FPixelStreamingInputDevice(InMessageHandler, InputComponents));
+	return InputDevice;
+}
+
+FPixelStreamingInputDevice& FPixelStreamingPlugin::GetInputDevice()
+{
+	return *InputDevice;
+}
+
+TSharedPtr<FPixelStreamingInputDevice> FPixelStreamingPlugin::GetInputDevicePtr()
+{
+	return InputDevice;
+}
+
+void FPixelStreamingPlugin::AddClientConfig(TSharedRef<FJsonObject>& JsonObject)
+{
+	checkf(InputDevice.IsValid(), TEXT("No Input Device available when populating Client Config"));
+
+	JsonObject->SetBoolField(TEXT("FakingTouchEvents"), InputDevice->IsFakingTouchEvents());
+
+	FString PixelStreamingControlScheme;
+	if (FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingControlScheme="), PixelStreamingControlScheme))
+	{
+		JsonObject->SetStringField(TEXT("ControlScheme"), PixelStreamingControlScheme);
 	}
 
-	void OnPreResizeWindowBackbuffer(void* BackBuffer)
+	float PixelStreamingFastPan;
+	if (FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingFastPan="), PixelStreamingFastPan))
 	{
-		if (Streamer)
+		JsonObject->SetNumberField(TEXT("FastPan"), PixelStreamingFastPan);
+	}
+}
+
+void FPixelStreamingPlugin::SendResponse(const FString& Descriptor)
+{
+	Streamer->SendResponse(Descriptor);
+}
+
+void FPixelStreamingPlugin::OnGameModePostLogin(AGameModeBase* GameMode, APlayerController* NewPlayer)
+{
+	UWorld* NewPlayerWorld = NewPlayer->GetWorld();
+	for (TObjectIterator<UPixelStreamingInputComponent> ObjIt; ObjIt; ++ObjIt)
+	{
+		UPixelStreamingInputComponent* InputComponent = *ObjIt;
+		UWorld* InputComponentWorld = InputComponent->GetWorld();
+		if (InputComponentWorld == NewPlayerWorld)
 		{
-			ENQUEUE_UNIQUE_RENDER_COMMAND_ONEPARAMETER(
-				FPixelStreamingOnPreResizeWindowBackbuffer,
-				FPixelStreamingPlugin*, Plugin, this,
-			{
-				Plugin->OnPreResizeWindowBackbuffer_RenderThread();
-			});	
-
-			// Make sure OnPreResizeWindowBackbuffer_RenderThread is executed before continuing
-			FlushRenderingCommands();
-		}
-	}
-
-	void OnPreResizeWindowBackbuffer_RenderThread()
-	{
-		Streamer->OnPreResizeWindowBackbuffer();
-	}
-
-	virtual TSharedPtr<class IInputDevice> CreateInputDevice(const TSharedRef<FGenericApplicationMessageHandler>& InMessageHandler) override
-	{
-		InputDevice = MakeShareable(new FPixelStreamingInputDevice(InMessageHandler, InputComponents));
-		return InputDevice;
-	}
-
-	virtual FPixelStreamingInputDevice& GetInputDevice() override
-	{
-		return *InputDevice;
-	}
-
-	virtual void AddClientConfig(TSharedRef<FJsonObject>& JsonObject) override
-	{
-		checkf(InputDevice.IsValid(), TEXT("No Input Device available when populating Client Config"));
-
-		JsonObject->SetBoolField(TEXT("FakingTouchEvents"), InputDevice->IsFakingTouchEvents());
-
-		FString PixelStreamingControlScheme;
-		if (FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingControlScheme="), PixelStreamingControlScheme))
-		{
-			JsonObject->SetStringField(TEXT("ControlScheme"), PixelStreamingControlScheme);
-		}
-
-		float PixelStreamingFastPan;
-		if (FParse::Value(FCommandLine::Get(), TEXT("PixelStreamingFastPan="), PixelStreamingFastPan))
-		{
-			JsonObject->SetNumberField(TEXT("FastPan"), PixelStreamingFastPan);
-		}
-	}
-
-	virtual void SendResponse(const FString& Descriptor) override
-	{
-		Streamer->SendResponse(Descriptor);
-	}
-
-	void OnGameModePostLogin(AGameModeBase* GameMode, APlayerController* NewPlayer)
-	{
-		UWorld* NewPlayerWorld = NewPlayer->GetWorld();
-		for (TObjectIterator<UPixelStreamingInputComponent> ObjIt; ObjIt; ++ObjIt)
-		{
-			UPixelStreamingInputComponent* InputComponent = *ObjIt;
-			UWorld* InputComponentWorld = InputComponent->GetWorld();
-			if (InputComponentWorld == NewPlayerWorld)
-			{
-				InputComponents.Push(InputComponent);
-			}
-		}
-		if (InputComponents.Num() == 0)
-		{
-			UPixelStreamingInputComponent* InputComponent = NewObject<UPixelStreamingInputComponent>(NewPlayer);
-			InputComponent->RegisterComponent();
 			InputComponents.Push(InputComponent);
 		}
 	}
-
-	void OnGameModeLogout(AGameModeBase* GameMode, AController* Exiting)
+	if (InputComponents.Num() == 0)
 	{
-		InputComponents.Empty();
+		UPixelStreamingInputComponent* InputComponent = NewObject<UPixelStreamingInputComponent>(NewPlayer);
+		InputComponent->RegisterComponent();
+		InputComponents.Push(InputComponent);
 	}
+}
 
-private:
-	TUniquePtr<FStreamer> Streamer;
-	FTexture2DRHIRef mResolvedFrameBuffer;
-	TSharedPtr<FPixelStreamingInputDevice> InputDevice;
-	TArray<UPixelStreamingInputComponent*> InputComponents;
-};
+void FPixelStreamingPlugin::OnGameModeLogout(AGameModeBase* GameMode, AController* Exiting)
+{
+	InputComponents.Empty();
+}
 
 IMPLEMENT_MODULE(FPixelStreamingPlugin, PixelStreaming)
