d813c342b4fc1626d9eeaac8b80725a2376a43ae
Ben Zeigler <Ben.Zeigler@epicgames.com>
2018-11-19 19:54:25 -0500

    #jira UE-66366 Fix crash when editing nested blueprint user structures #rb michael.noland #lockdown nick.penwarden
    [CL 4581002 by Ben Zeigler in 4.21 branch]

--- Engine/Source/Editor/Kismet/Private/UserDefinedStructureEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/UserDefinedStructureEditor.cpp
@@ -152,7 +152,9 @@ public:
 		if (Info != FStructureEditorUtils::DefaultValueChanged)
 		{
 			StructData->Initialize(UserDefinedStruct.Get());
-			DetailsView->SetObject(UserDefinedStruct.Get());
+
+			// Force the set object call because we may be called multiple times in a row if more than one struct was changed at the same time
+			DetailsView->SetObject(UserDefinedStruct.Get(), true);
 		}
 
 		UserDefinedStruct.Get()->InitializeDefaultValue(StructData->GetStructMemory());
