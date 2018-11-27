a532fe5cb69ff19bbb633c398fc05d3723e5efbe
Ben Marsh <Ben.Marsh@epicgames.com>
2018-11-27 15:23:23 -0500

    UGS: Update version to 1.166
    #rb none
    #rnx
    
    [CL 4598671 by Ben Marsh in Dev-Build branch]

--- Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
+++ Engine/Source/Programs/UnrealGameSync/UnrealGameSync/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.165.*")]
-[assembly: AssemblyFileVersion("1.165.0.0")]
+[assembly: AssemblyVersion("1.166.*")]
+[assembly: AssemblyFileVersion("1.166.0.0")]
