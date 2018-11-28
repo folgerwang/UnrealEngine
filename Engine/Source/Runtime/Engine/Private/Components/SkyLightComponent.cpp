69aa94bfa4c89a2996be8ce822076c595002716f
Marcus Wassmer <Marcus.Wassmer@epicgames.com>
2018-11-28 15:25:45 -0500

    Fix skylight hiding/showing #jira UE-66870 #rb none #lockdown rolando.caloca
    [CL 4604523 by Marcus Wassmer in Dev-Rendering branch]

--- Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SkyLightComponent.cpp
@@ -249,17 +249,10 @@ void USkyLightComponent::CreateRenderState_Concurrent()
 {
 	Super::CreateRenderState_Concurrent();
 
-	bool bHidden = false;
-#if WITH_EDITORONLY_DATA
-	bHidden = GetOwner() ? GetOwner()->bHiddenEdLevel : false;
-#endif // WITH_EDITORONLY_DATA
-
-	if(!ShouldComponentAddToScene())
-	{
-		bHidden = true;
-	}
-
+	bool bHidden = !ShouldComponentAddToScene() || !ShouldRender();
 	const bool bIsValid = SourceType != SLS_SpecifiedCubemap || Cubemap != NULL;
+
+
 
 	if (bAffectsWorld && bVisible && !bHidden && bIsValid)
 	{
