78b19ddb99aac19f67d94b6ec5c1aa83e8e88262
Brandon Schaefer <Brandon.Schaefer@epicgames.com>
2018-11-28 15:10:52 -0500

    Linux: Enclose bash variable to allow spaces in paths. PR #5306 (thanks AlSafty!)
    #jira UE-66922
    #rb none
    #fyi Arciel.Rekman
    #lockdown Cristina.Riveron
    
    [CL 4604418 by Brandon Schaefer in 4.21 branch]

--- Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
+++ Engine/Build/BatchFiles/Linux/GenerateProjectFiles.sh
@@ -20,7 +20,7 @@ if [ ! -d "$BASE_PATH/../../../Source" ]; then
  exit 1
 fi
 
-source "$BASE_PATH/SetupMono.sh" $BASE_PATH
+source "$BASE_PATH/SetupMono.sh" "$BASE_PATH"
 
 if [ -f "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" ]; then
 	xbuild "$BASE_PATH/../../../Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj" /property:Configuration="Development" /p:TargetFrameworkVersion=v4.5 /verbosity:quiet /nologo
