fb6eea73538fa25e5b8d55a6d524a6a104d7c52f
Jamie Dale <Jamie.Dale@epicgames.com>
2018-11-30 11:46:46 -0500

    Fixed double offset of map values when ARO'ing from Python
    #jira UE-67050
    #rb Conan.Reis
    #lockdown Cristina.Riveron
    
    [CL 4616241 by Jamie Dale in 4.21 branch]

--- Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyReferenceCollector.cpp
+++ Engine/Plugins/Experimental/PythonScriptPlugin/Source/PythonScriptPlugin/Private/PyReferenceCollector.cpp
@@ -307,8 +307,9 @@ void FPyReferenceCollector::AddReferencedObjectsFromPropertyInternal(FReferenceC
 				{
 					if (ScriptMapHelper.IsValidIndex(SparseElementIndex))
 					{
-						AddReferencedObjectsFromPropertyInternal(InCollector, ScriptMapHelper.GetKeyProperty(), ScriptMapHelper.GetKeyPtr(SparseElementIndex), InFlags, bMapKeysChanged);
-						AddReferencedObjectsFromPropertyInternal(InCollector, ScriptMapHelper.GetValueProperty(), ScriptMapHelper.GetValuePtr(SparseElementIndex), InFlags, bMapValuesChanged);
+						// Note: We use the pair pointer below as AddReferencedObjectsFromPropertyInternal expects a base address and the key/value property will apply the correct offset from the base
+						AddReferencedObjectsFromPropertyInternal(InCollector, ScriptMapHelper.GetKeyProperty(), ScriptMapHelper.GetPairPtr(SparseElementIndex), InFlags, bMapKeysChanged);
+						AddReferencedObjectsFromPropertyInternal(InCollector, ScriptMapHelper.GetValueProperty(), ScriptMapHelper.GetPairPtr(SparseElementIndex), InFlags, bMapValuesChanged);
 					}
 				}
 
