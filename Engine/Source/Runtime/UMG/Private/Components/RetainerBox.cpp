3f7d7a49a843b1341c0bc8bf8af691be6ae007a7
Dzuelu <Dzuelu@users.noreply.github.com>
2018-12-03 17:13:52 -0500

    PR #5315: Update RetainerBox SetRenderingPhase function (Contributed by Dzuelu)
    #rb Chris.Gagnon
    #codreview Nick.Darnell
    
    [CL 4625592 by Chris Gagnon in Dev-Editor branch]

--- Engine/Source/Runtime/UMG/Private/Components/RetainerBox.cpp
+++ Engine/Source/Runtime/UMG/Private/Components/RetainerBox.cpp
@@ -28,6 +28,11 @@ void URetainerBox::SetRenderingPhase(int PhaseToRenderOn, int32 TotalRenderingPh
 {
 	Phase = PhaseToRenderOn;
 	PhaseCount = TotalRenderingPhases;
+	
+	if ( PhaseCount < 1 )
+	{
+		PhaseCount = 1;
+	}
 
 	if (MyRetainerWidget.IsValid())
 	{
