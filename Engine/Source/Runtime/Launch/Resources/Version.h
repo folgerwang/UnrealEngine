ce0ef78dea2678d29c77490ad3cc2f8bcf18ce9e
Will Fissler <Will.Fissler@epicgames.com>
2018-12-05 10:44:54 -0500

    Updating versioning files to 4.21.2.
    #jira none
    #rb Ben.Marsh
    #lockdown Cristina.Riveron
    
    [CL 4632181 by Will Fissler in 4.21 branch]

--- Engine/Source/Runtime/Launch/Resources/Version.h
+++ Engine/Source/Runtime/Launch/Resources/Version.h
@@ -8,55 +8,55 @@
 //
 // The following concepts are used for versioning in UE4:
 //
-//  -  The *engine version* defines the explicit major/minor/patch version of the engine, plus the changelist and branch name that it was built from. The 
+//  -  The *engine version* defines the explicit major/minor/patch version of the engine, plus the changelist and branch name that it was built from. The
 //     changelist is assumed to be a monotonically increasing number in the current branch, and is used both as a unique identifier and to infer that one engine
-//     was later than another. Tagged property serialization in UE4 is tolerant to properties being added or removed, so we always want to prevent an older 
+//     was later than another. Tagged property serialization in UE4 is tolerant to properties being added or removed, so we always want to prevent an older
 //     build of the engine loading assets created with a newer build, discarding properties which have recently been added, and silently losing data
-//     when the asset is saved out. The changelist allows ordering versions in such cases. The engine version is encapsulated by the FEngineVersion class, of 
+//     when the asset is saved out. The changelist allows ordering versions in such cases. The engine version is encapsulated by the FEngineVersion class, of
 //     which there are two commonly referenced instances:
 //
-//         FEngineVersion::Current()        normally uses ENGINE_CURRENT_CL_VERSION for the changelist component, and indicates the code the engine was built 
+//         FEngineVersion::Current()        normally uses ENGINE_CURRENT_CL_VERSION for the changelist component, and indicates the code the engine was built
 //                                          from. This is typically only used for diagnostic and display purposes.
 //
-//         FEngineVersion::CompatibleWith() normally uses ENGINE_COMPATIBLE_CL_VERSION for the changelist component and '0' for the patch component, and 
-//                                          indicates the baseline version of the engine that this build maintains strict binary compatibility with. 
-//                                          By default, this compatibility extends to assets, executable modules, and any network data transmitted between 
+//         FEngineVersion::CompatibleWith() normally uses ENGINE_COMPATIBLE_CL_VERSION for the changelist component and '0' for the patch component, and
+//                                          indicates the baseline version of the engine that this build maintains strict binary compatibility with.
+//                                          By default, this compatibility extends to assets, executable modules, and any network data transmitted between
 //                                          two builds, and is used when creating patches and hotfixes that can be used interchangably with another build.
 //                                          This should be used for versioning in the majority of cases in the engine.
 //
-//     Both the ENGINE_CURRENT_CL_VERSION and ENGINE_COMPATIBLE_CL_VERSION macros can be updated systemically by build systems using the UpdateLocalVersion 
+//     Both the ENGINE_CURRENT_CL_VERSION and ENGINE_COMPATIBLE_CL_VERSION macros can be updated systemically by build systems using the UpdateLocalVersion
 //     AutomationTool command (as well as the ENGINE_IS_LICENSEE_VERSION and BRANCH_NAME macros).
 //
-//  -  The *object version* (aka serialization version) is as a monotonically incrementing (but manually updated) integer, and is used to write one-way 
-//     upgrade code in custom UObject serialization functions. It is set by the enum in ObjectVersion.h, and is global to the whole engine. This version number 
-//     is saved as a raw integer value in package headers, so it cannot be safely reordered or merged between branches. It should ONLY be updated 
+//  -  The *object version* (aka serialization version) is as a monotonically incrementing (but manually updated) integer, and is used to write one-way
+//     upgrade code in custom UObject serialization functions. It is set by the enum in ObjectVersion.h, and is global to the whole engine. This version number
+//     is saved as a raw integer value in package headers, so it cannot be safely reordered or merged between branches. It should ONLY be updated
 //     by Epic, otherwise future engine merges may corrupt content.
 //
 //  -  The *licensee object version* is provided for licensees to create their own one-way upgrade paths akin to the regular object version. Epic will never
 //     add entries to this enumeration. It is defined by the enum in ObjectVersion.h
 //
-//  -  Any number of *custom object version* objects may be registered to create orthoganal incrementing version numbers similar to the object version and 
-//     licensee version enums (see FCustomVersion). Each one is registered with a GUID, ensuring uniqueness and allowing the FArchive to quickly store and 
+//  -  Any number of *custom object version* objects may be registered to create orthoganal incrementing version numbers similar to the object version and
+//     licensee version enums (see FCustomVersion). Each one is registered with a GUID, ensuring uniqueness and allowing the FArchive to quickly store and
 //     retrieve them without any context of what they represent. Custom versions may be created for individual projects, subsystems, or branches.
 //
-//  -  The *build version* is an opaque string specific to the product being built, and should be used for identifying the current application 
-//     (as opposed to distinct applications built with the same engine version). It is set by the BUILD_VERSION macro, which can be updated using the 
+//  -  The *build version* is an opaque string specific to the product being built, and should be used for identifying the current application
+//     (as opposed to distinct applications built with the same engine version). It is set by the BUILD_VERSION macro, which can be updated using the
 //     UpdateLocalVersion AutomationTool command.
 //
 //  -  The *network version* and *replay version* are used for versioning the network and replay subsystems, and default to the compatible engine version.
 //
-//  -  The *engine association* in a .uproject file often takes the appearance of a version number for launcher-installed binary UE4 releases, but may be 
+//  -  The *engine association* in a .uproject file often takes the appearance of a version number for launcher-installed binary UE4 releases, but may be
 //     other identifiers as well. See ProjectDescriptor.h for a description of how this technique works.
 //
-// Constants in this file are updated by AutomationTool and UnrealGameSync. Be careful when changing formatting for the submitted version of this file that 
+// Constants in this file are updated by AutomationTool and UnrealGameSync. Be careful when changing formatting for the submitted version of this file that
 // these tools can still parse it.
 //=============================================================================================================================================================
 
-// These numbers define the banner UE4 version, and are the most significant numbers when ordering two engine versions (that is, a 4.12.* version is always 
+// These numbers define the banner UE4 version, and are the most significant numbers when ordering two engine versions (that is, a 4.12.* version is always
 // newer than a 4.11.* version, regardless of the changelist that it was built with)
 #define ENGINE_MAJOR_VERSION	4
 #define ENGINE_MINOR_VERSION	21
-#define ENGINE_PATCH_VERSION	1
+#define ENGINE_PATCH_VERSION	2
 
 // Macros for encoding strings
 #define VERSION_TEXT(x) TEXT(x)
