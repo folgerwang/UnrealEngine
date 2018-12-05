51bdcbdfe9a1646768a950fcfd04f27939939912
Rolando Caloca <Rolando.Caloca@epicgames.com>
2018-12-05 09:09:35 -0500

    DR - Copy from 4629036 Static analysis fix #rb none #rnx
    [CL 4631853 by Rolando Caloca in Dev-Rendering branch]

--- Engine/Source/Runtime/RenderCore/Public/PixelShaderUtils.h
+++ Engine/Source/Runtime/RenderCore/Public/PixelShaderUtils.h
@@ -32,6 +32,7 @@ struct RENDERCORE_API FPixelShaderUtils
 		const TShaderClass* PixelShader,
 		const typename TShaderClass::FParameters& Parameters)
 	{
+		CA_ASSUME(PixelShader);
 		TShaderMapRef<FVisualizeTextureVS> VertexShader(GlobalShaderMap);
 
 		FGraphicsPipelineStateInitializer GraphicsPSOInit;
@@ -60,6 +61,7 @@ struct RENDERCORE_API FPixelShaderUtils
 		const TShaderClass* PixelShader,
 		typename TShaderClass::FParameters* Parameters)
 	{
+		CA_ASSUME(PixelShader != nullptr);
 		ClearUnusedGraphResources(PixelShader, Parameters);
 
 		GraphBuilder.AddPass(
