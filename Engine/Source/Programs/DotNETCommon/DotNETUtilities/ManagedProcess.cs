883be04367e12cefbfd8422146c8ba026e9df55e
Chad Garyet <Chad.Garyet@epicgames.com>
2018-12-11 14:34:14 -0500

    Wrap setting process priority in a try/catch in case the process exists before the priority is able to be set #jira none #rb none
    [CL 4649394 by Chad Garyet in Dev-Build branch]

--- Engine/Source/Programs/DotNETCommon/DotNETUtilities/ManagedProcess.cs
+++ Engine/Source/Programs/DotNETCommon/DotNETUtilities/ManagedProcess.cs
@@ -483,10 +483,16 @@ namespace Tools.DotNETCommon
 				}
 
 				FrameworkProcess.Start();
-				FrameworkProcess.PriorityClass = Priority;
+				try
+				{
+					FrameworkProcess.PriorityClass = Priority;
+				}
+				catch
+				{
 
+				}
 				// If we have any input text, write it to stdin now
-				if(Input != null)
+				if (Input != null)
 				{
 					FrameworkProcess.StandardInput.BaseStream.Write(Input, 0, Input.Length);
 					FrameworkProcess.StandardInput.BaseStream.Close();
