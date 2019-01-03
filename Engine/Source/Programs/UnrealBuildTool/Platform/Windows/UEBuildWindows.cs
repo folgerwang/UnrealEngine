5a7613527a789710c4c0b89cd34f982a46166bd4
Ben Marsh <Ben.Marsh@epicgames.com>
2019-01-02 23:24:57 -0500

    UBT: Update default Clang version to 7.10.
    #rb none
    #rnx
    
    [CL 4677740 by Ben Marsh in Dev-Build branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/Windows/UEBuildWindows.cs
@@ -401,7 +401,7 @@ namespace UnrealBuildTool
 		/// <summary>
 		/// The default compiler version to be used, if installed. 
 		/// </summary>
-		static readonly VersionNumber DefaultClangToolChainVersion = VersionNumber.Parse("6.0.0");
+		static readonly VersionNumber DefaultClangToolChainVersion = VersionNumber.Parse("7.1.0");
 
 		/// <summary>
 		/// The default compiler version to be used, if installed. 
