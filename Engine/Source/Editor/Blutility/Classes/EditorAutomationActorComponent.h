5425d1b69e02c734336dec605d63d07b7efc3e2c
Lauren Ridge <Lauren.Ridge@epicgames.com>
2019-01-03 17:08:49 -0500

    Update copyright #jira none #rb none
    [CL 4680041 by Lauren Ridge in Dev-Editor branch]

--- Engine/Source/Editor/Blutility/Classes/EditorAutomationActorComponent.h
+++ Engine/Source/Editor/Blutility/Classes/EditorAutomationActorComponent.h
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 /**
  * Base class of any editor-only automation test actor components
