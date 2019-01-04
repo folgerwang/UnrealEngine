10bc7cabc067a810f3be1cc7cba6ecd774609452
Marc Audy <Marc.Audy@epicgames.com>
2019-01-04 14:40:26 -0500

    Prevent crash clearing instance components if there happens to be a null in the array #jira UE-68015 #rb #lockdown Cristina.Riveron
    [CL 4682410 by Marc Audy in 4.21 branch]

--- Engine/Source/Runtime/Engine/Private/Actor.cpp
+++ Engine/Source/Runtime/Engine/Private/Actor.cpp
@@ -2643,7 +2643,10 @@ void AActor::ClearInstanceComponents(const bool bDestroyComponents)
 		// Run in reverse to reduce memory churn when the components are removed from InstanceComponents
 		for (int32 Index=CachedComponents.Num()-1; Index >= 0; --Index)
 		{
-			CachedComponents[Index]->DestroyComponent();
+			if (CachedComponents[Index])
+			{
+				CachedComponents[Index]->DestroyComponent();
+			}
 		}
 	}
 	else
