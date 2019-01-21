584390f09403d865f279517750c13f0f53e272ac
dan hertzka <Dan.Hertzka@epicgames.com>
2019-01-21 14:52:26 -0500

    RE-re-resolving borked merge from yesterday, should fix compile
    #rb none
    #jira none
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: dan.hertzka
    #ROBOMERGE-SOURCE: CL 4763485 in //UE4/Main/...
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 4763486 by dan hertzka in Dev-Networking branch]

--- Engine/Source/Runtime/UMG/Private/WidgetTree.cpp
+++ Engine/Source/Runtime/UMG/Private/WidgetTree.cpp
@@ -67,6 +67,63 @@ UPanelWidget* UWidgetTree::FindWidgetParent(UWidget* Widget, int32& OutChildInde
 	return Parent;
 }
 
+>>>> ORIGINAL //UE4/Main/Engine/Source/Runtime/UMG/Private/WidgetTree.cpp#none
+==== THEIRS //UE4/Main/Engine/Source/Runtime/UMG/Private/WidgetTree.cpp@=4753498
+UWidget* UWidgetTree::FindWidgetChild(UPanelWidget* ParentWidget, FName ChildWidgetName, int32& OutChildIndex)
+{
+	OutChildIndex = INDEX_NONE;
+	UWidget* FoundChild = nullptr;
+
+	if (ParentWidget)
+	{
+		const auto CheckWidgetFunc = [&FoundChild, ChildWidgetName](UWidget* Widget) 
+			{
+				if (!FoundChild && Widget->GetFName() == ChildWidgetName)
+				{
+					FoundChild = Widget;
+				}
+			};
+
+		// Check all the children of the given ParentWidget, but only track the index at the top level
+		for (int32 ChildIdx = 0; ChildIdx < ParentWidget->GetChildrenCount(); ++ChildIdx)
+		{
+			CheckWidgetFunc(ParentWidget->GetChildAt(ChildIdx));
+
+			if (!FoundChild)
+			{
+				ForWidgetAndChildren(ParentWidget->GetChildAt(ChildIdx), CheckWidgetFunc);
+			}
+
+			if (FoundChild)
+			{
+				OutChildIndex = ChildIdx;
+				break;
+			}
+		}
+	}
+
+	return FoundChild;
+}
+
+int32 UWidgetTree::FindChildIndex(const UPanelWidget* ParentWidget, const UWidget* ChildWidget)
+{
+	const UWidget* CurrentWidget = ChildWidget;
+	while (CurrentWidget)
+	{
+		const UPanelWidget* NextParent = CurrentWidget->Slot ? CurrentWidget->Slot->Parent : nullptr;
+		if (NextParent && NextParent == ParentWidget)
+		{
+			return NextParent->GetChildIndex(CurrentWidget);
+		}
+
+		CurrentWidget = NextParent;
+	}
+
+	return INDEX_NONE;
+}
+
+==== YOURS //DanH_FortSpecial/Engine/Source/Runtime/UMG/Private/WidgetTree.cpp
+<<<<
 bool UWidgetTree::RemoveWidget(UWidget* InRemovedWidget)
 {
 	bool bRemoved = false;
