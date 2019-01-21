464104b13a564e9f8107e6a4cb3186ebfeeff53f
zak middleton <Zak.Middleton@epicgames.com>
2019-01-21 13:30:55 -0500

    #ue4 - Fix cached rotator/quat conversion getting out of sync in rare cases where FScopedPreventAttachedComponentMove was used and the component rotation changed within the scope. This could happen for simulated characters running RootMotion.
    #jira UE-68791
    #udn https://udn.unrealengine.com/questions/478413/but-in-smooth-mesh-locomotion-on-simulated-proxy.html
    
    #ROBOMERGE-OWNER: ryan.gerleve
    #ROBOMERGE-AUTHOR: zak.middleton
    #ROBOMERGE-SOURCE: CL 4762849 via CL 4762852 via CL 4762855
    #ROBOMERGE-BOT: ENGINE (Main -> Dev-Networking)
    
    [CL 4762856 by zak middleton in Dev-Networking branch]

--- Engine/Source/Runtime/Engine/Private/Components/SceneComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SceneComponent.cpp
@@ -3362,7 +3362,7 @@ FScopedPreventAttachedComponentMove::~FScopedPreventAttachedComponentMove()
 			}
 			if (!bSavedAbsoluteRotation)
 			{
-				Owner->RelativeRotation = ChildRelativeTM.GetRotation().Rotator();
+				Owner->RelativeRotation = Owner->RelativeRotationCache.QuatToRotator(ChildRelativeTM.GetRotation());
 			}
 			if (!bSavedAbsoluteScale)
 			{
