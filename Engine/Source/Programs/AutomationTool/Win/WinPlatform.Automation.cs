ecae91bc939a7cd28c700b1a7c70f18abd882d1c
daniel lamb <Daniel.Lamb@epicgames.com>
2019-01-28 20:38:12 -0500

    Enabled compression on first 4 chunks for windows. Enabled custom compression on windows. Prevented ChunkDBs from being uploaded to network drive now are uploaded straight to cloud.
    #ROBOMERGE-SOURCE: CL 4823230 via CL 4825401
    
    [CL 4826399 by daniel lamb in Main branch]

--- Engine/Source/Programs/AutomationTool/Win/WinPlatform.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Win/WinPlatform.Automation.cs
@@ -174,11 +174,19 @@ public abstract class BaseWinPlatform : Platform
 	{
 		return "Windows";
 	}
+	
+	public override string GetPlatformPakCommandLine(ProjectParams Params, DeploymentContext SC)
+	{
+		string PakParams = " -patchpaddingalign=2048";
 
-    public override string GetPlatformPakCommandLine(ProjectParams Params, DeploymentContext SC)
-    {
-        return " -patchpaddingalign=2048";
-    }
+		string OodleDllPath = DirectoryReference.Combine(SC.ProjectRoot, "Binaries/ThirdParty/Oodle/Win64/UnrealPakPlugin.dll").FullName;
+		if (File.Exists(OodleDllPath))
+		{
+			PakParams += String.Format(" -customcompressor=\"{0}\"", OodleDllPath);
+		}
+
+		return PakParams;
+	}
 
 	public override void Package(ProjectParams Params, DeploymentContext SC, int WorkingCL)
 	{
