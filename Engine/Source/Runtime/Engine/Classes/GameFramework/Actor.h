08be3724b45eb868f32b3f2325cc21175f33bd9b
chris gagnon <Chris.Gagnon@epicgames.com>
2019-01-31 04:46:20 -0500

    Reverted change that caused this regression.
    #rb none
    
    #ROBOMERGE-SOURCE: CL 4850445 in //UE4/Dev-Editor/...
    #ROBOMERGE-BOT: ENGINE (Dev-Editor -> Main)
    
    [CL 4855679 by chris gagnon in Main branch]

--- Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
+++ Engine/Source/Runtime/Engine/Classes/GameFramework/Actor.h
@@ -618,7 +618,7 @@ public:
 	uint8 bIsEditorPreviewActor:1;
 
 	/** Whether this actor is hidden by the layer browser. */
-	UPROPERTY(Transient)
+	UPROPERTY()
 	uint8 bHiddenEdLayer:1;
 
 	/** Whether this actor is hidden by the level browser. */
