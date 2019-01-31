c05c8e0ff1f121c15d525c06e4c96555b1ea7ead
dan oconnor <Dan.OConnor@epicgames.com>
2019-01-30 19:01:05 -0500

    Added logging to ForceDelete operation #jira UE-58891 #rb Phillip.Kavan
    #ROBOMERGE-SOURCE: CL 4848460 via CL 4851899
    
    [CL 4852037 by dan oconnor in Main branch]

--- Engine/Source/Editor/UnrealEd/Private/ObjectTools.cpp
+++ Engine/Source/Editor/UnrealEd/Private/ObjectTools.cpp
@@ -2236,6 +2236,19 @@ namespace ObjectTools
 			return 0;
 		}
 
+		{
+			// Force delete is a dangerous operation, add some fingerprints to the log:
+			FString Msg;
+			Msg.Append(FString::Printf(TEXT("Force Deleting %d Package(s):"), ShownObjectsToDelete.Num()));
+			const int32 MAX_PACKAGES_TO_LOG = 10;
+			for(int32 I = 0; I < ShownObjectsToDelete.Num() && I < MAX_PACKAGES_TO_LOG; ++I)
+			{
+				Msg.Append(TEXT("\n"));
+				Msg.Append(FString::Printf(TEXT("\tAsset Name: %s"), *GetPathNameSafe(ShownObjectsToDelete[I])));
+			}
+			UE_LOG(LogUObjectGlobals, Log, TEXT("%s"), *Msg);
+		}
+
 		GWarn->BeginSlowTask( NSLOCTEXT("UnrealEd", "Deleting", "Deleting"), true );
 
 		struct FSCSNodeToDelete
