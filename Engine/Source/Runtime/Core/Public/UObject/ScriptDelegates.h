2ee214ce65e365fbfd6cd2b1dd85987cf38f31b7
Marc Audy <Marc.Audy@epicgames.com>
2019-02-05 07:52:13 -0500

    RemoveAll should take a const UObject* #jira #rb #rnx
    [CL 4897425 by Marc Audy in 4.22 branch]

--- Engine/Source/Runtime/Core/Public/UObject/ScriptDelegates.h
+++ Engine/Source/Runtime/Core/Public/UObject/ScriptDelegates.h
@@ -381,7 +381,7 @@ public:
 	 *
 	 * @param InObject The object to remove bindings for.
 	 */
-	void RemoveAll( UObject* Object )
+	void RemoveAll(const UObject* Object)
 	{
 		for (int32 BindingIndex = InvocationList.Num() - 1; BindingIndex >= 0; --BindingIndex)
 		{
