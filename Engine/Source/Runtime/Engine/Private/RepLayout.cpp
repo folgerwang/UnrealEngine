111ea1eed5dd5df5ade0bb14e61568ace3dd48f6
Jason Adcock <jason.adcock@xa.epicgames.com>
2019-02-05 17:15:43 -0500

    Fix for crash in FRepLayout::InitFromObjectClass() #jira UE-54480 #RB ryan.gerleve
    [CL 4905599 by Jason Adcock in 4.22 branch]

--- Engine/Source/Runtime/Engine/Private/RepLayout.cpp
+++ Engine/Source/Runtime/Engine/Private/RepLayout.cpp
@@ -4140,12 +4140,13 @@ void FRepLayout::InitFromObjectClass(UClass* InObjectClass, const UNetConnection
 	SCOPE_CYCLE_COUNTER(STAT_RepLayout_InitFromObjectClass);
 	SCOPE_CYCLE_UOBJECT(ObjectClass, InObjectClass);
 
-	RoleIndex = -1;
-	RemoteRoleIndex = -1;
-	FirstNonCustomParent = -1;
+	const bool bIsObjectActor = InObjectClass->IsChildOf(AActor::StaticClass());
+	RoleIndex                 = -1;
+	RemoteRoleIndex           = -1;
+	FirstNonCustomParent      = -1;
 
-	int32 RelativeHandle = 0;
-	int32 LastOffset = -1;
+	int32 RelativeHandle      = 0;
+	int32 LastOffset          = -1;
 
 	InObjectClass->SetUpRuntimeReplicationData();
 	Parents.Empty(InObjectClass->ClassReps.Num());
@@ -4187,19 +4188,22 @@ void FRepLayout::InitFromObjectClass(UClass* InObjectClass, const UNetConnection
 			FirstNonCustomParent = ParentHandle;
 		}
 
-		// Find Role/RemoteRole property indexes so we can swap them on the client
-		if (Property->GetFName() == NAME_Role)
+		if (bIsObjectActor)
 		{
-			check(RoleIndex == -1);
-			check(Parents[ParentHandle].CmdEnd == Parents[ParentHandle].CmdStart + 1);
-			RoleIndex = ParentHandle;
-		}
+			// Find Role/RemoteRole property indexes so we can swap them on the client
+			if (Property->GetFName() == NAME_Role)
+			{
+				check(RoleIndex == -1);
+				check(Parents[ParentHandle].CmdEnd == Parents[ParentHandle].CmdStart + 1);
+				RoleIndex = ParentHandle;
+			}
 
-		if (Property->GetFName() == NAME_RemoteRole)
-		{
-			check(RemoteRoleIndex == -1);
-			check(Parents[ParentHandle].CmdEnd == Parents[ParentHandle].CmdStart + 1);
-			RemoteRoleIndex = ParentHandle;
+			if (Property->GetFName() == NAME_RemoteRole)
+			{
+				check(RemoteRoleIndex == -1);
+				check(Parents[ParentHandle].CmdEnd == Parents[ParentHandle].CmdStart + 1);
+				RemoteRoleIndex = ParentHandle;
+			}
 		}
 	}
 
