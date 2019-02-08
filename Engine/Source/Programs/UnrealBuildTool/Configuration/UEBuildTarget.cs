322f30c289d891e4836703ca33f6b712e9eb15cb
Ben Marsh <Ben.Marsh@epicgames.com>
2019-02-08 17:08:35 -0500

    UBT: Prevent hot reload attempting to build non-C++ modules.
    #rb none
    #jira UE-69518
    
    [CL 4952727 by Ben Marsh in 4.22 branch]

--- Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildTarget.cs
+++ Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildTarget.cs
@@ -2035,7 +2035,7 @@ namespace UnrealBuildTool
 				{
 					if(!UnrealBuildTool.IsProjectInstalled() || EnabledPlugins.Where(x => x.Type == PluginType.Mod).Any(x => Binary.OutputFilePaths[0].IsUnderDirectory(x.Directory)))
 					{
-						HotReloadModuleNames.UnionWith(GameModules.Select(x => x.Name));
+						HotReloadModuleNames.UnionWith(GameModules.OfType<UEBuildModuleCPP>().Select(x => x.Name));
 					}
 				}
 			}
