ad294f56848fbe4ca8a9a6503ab49b2198a79088
Arciel Rekman <arciel.rekman@epicgames.com>
2019-02-11 17:35:45 -0500

    PhysX: change v13 Linux toolchain to v12 one.
    - There are issues when the libraries are compiled with clang 7.0.1 (tracked as UE-69848).
    
    #jira UE-69848
    #rb Ben.Marsh
    #rnx
    
    [CL 4965320 by Arciel Rekman in 4.22 branch]

--- Engine/Source/Programs/AutomationTool/Scripts/BuildPhysX.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/BuildPhysX.Automation.cs
@@ -650,6 +650,22 @@ class BuildPhysX : BuildCommand
 					{
 						LogInformation("Generating projects for lib " + TargetLib.ToString() + ", " + TargetData.ToString());
 
+						if (TargetData.Platform == UnrealTargetPlatform.Linux)
+						{
+							// the libraries are broken when compiled with clang 7.0.1
+							string OriginalToolchainPath = Environment.GetEnvironmentVariable("LINUX_MULTIARCH_ROOT");
+							if (!string.IsNullOrEmpty(OriginalToolchainPath))
+							{
+								string ToolchainPathToUse = OriginalToolchainPath.Replace("v13_clang-7.0.1-centos7", "v12_clang-6.0.1-centos7");
+								LogInformation("Working around problems with newer clangs: {0} -> {1}", OriginalToolchainPath, ToolchainPathToUse);
+								Environment.SetEnvironmentVariable("LINUX_MULTIARCH_ROOT", ToolchainPathToUse);
+							}
+							else
+							{
+								LogWarning("LINUX_MULTIARCH_ROOT is not set!");
+							}
+						}
+
 						ProcessStartInfo StartInfo = new ProcessStartInfo();
 						StartInfo.FileName = CMakeName;
 						StartInfo.WorkingDirectory = CMakeTargetDirectory.ToString();
