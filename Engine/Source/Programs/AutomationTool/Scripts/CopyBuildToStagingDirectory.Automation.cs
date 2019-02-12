4edd6d019fb1ac88401cc96b559506897caa80fd
Ben Marsh <Ben.Marsh@epicgames.com>
2019-02-11 20:48:44 -0500

    Fix ue4commandline.txt being written to engine directory during staging.
    #rb none
    #jira UE-69193
    
    [CL 4967031 by Ben Marsh in 4.22 branch]

--- Engine/Source/Programs/AutomationTool/Scripts/CopyBuildToStagingDirectory.Automation.cs
+++ Engine/Source/Programs/AutomationTool/Scripts/CopyBuildToStagingDirectory.Automation.cs
@@ -2674,7 +2674,7 @@ public partial class Project : CommandUtils
 
 	private static DirectoryReference GetIntermediateCommandlineDir(DeploymentContext SC)
 	{
-		return DirectoryReference.Combine(SC.EngineRoot, "Intermediate", "UAT", SC.FinalCookPlatform);
+		return DirectoryReference.Combine(SC.ProjectRoot, "Intermediate", "UAT", SC.FinalCookPlatform);
 	}
 
 	public static void WriteStageCommandline(FileReference IntermediateCmdLineFile, ProjectParams Params, DeploymentContext SC)
