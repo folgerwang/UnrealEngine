31264e71aa9b81af383fa27df4f298ea2c3f58ad
Ryan Vance <Ryan.Vance@epicgames.com>
2019-02-14 15:08:09 -0500

    #jira UE-67799 #rb trivial Adding TrackedDeviceClass_DisplayRedirect as a known steam vr tracked device type.
    [CL 4997825 by Ryan Vance in 4.22 branch]

--- Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRController/Private/SteamVRController.cpp
+++ Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRController/Private/SteamVRController.cpp
@@ -926,6 +926,8 @@ private:
 			case vr::TrackedDeviceClass_HMD:
 				// falls through
 			case vr::TrackedDeviceClass_TrackingReference:
+				// falls through
+			case vr::TrackedDeviceClass_DisplayRedirect:
 				break;
 			default:
 				UE_LOG(LogSteamVRController, Warning, TEXT("Encountered unsupported device class of %i!"), (int32)DeviceClass);
