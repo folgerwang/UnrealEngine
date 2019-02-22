1c5c33bdf984f1e76d6789b8cf27b94766a68781
David Hibbitts <David.Hibbitts@epicgames.com>
2019-02-22 12:00:37 -0500

    SceneComponent now broadcasts OnVisibilityChanged and OnHiddenInGameChanged events when bVisible or bHiddenInGame is toggled from the editor details panel
    #jira UE-70407
    #rb none
    
    [CL 5146085 by David Hibbitts in 4.22 branch]

--- Engine/Source/Runtime/Engine/Private/Components/SceneComponent.cpp
+++ Engine/Source/Runtime/Engine/Private/Components/SceneComponent.cpp
@@ -508,6 +508,14 @@ void USceneComponent::PostEditChangeProperty(FPropertyChangedEvent& PropertyChan
 	{
 		UpdateAttachedIsEditorOnly(this);
 	}
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(USceneComponent, bVisible))
+	{
+		OnVisibilityChanged();
+	}
+	else if (PropertyName == GET_MEMBER_NAME_CHECKED(USceneComponent, bHiddenInGame))
+	{
+		OnHiddenInGameChanged();
+	}
 
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 
