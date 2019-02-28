454bb9257edff667aa8725df585a58e20aea048e
Daniel Wright <Daniel.Wright@epicgames.com>
2019-02-28 17:19:36 -0500

    Fixed bSetupResourceClusters only getting reset if there was valid lighting in the map build data. #rb none #jira UE-70720
    [CL 5247082 by Daniel Wright in 4.22 branch]

--- Engine/Source/Runtime/Engine/Private/MapBuildData.cpp
+++ Engine/Source/Runtime/Engine/Private/MapBuildData.cpp
@@ -676,6 +676,8 @@ void UMapBuildDataRegistry::InvalidateStaticLighting(UWorld* World, bool bRecrea
 
 		MarkPackageDirty();
 	}
+
+	bSetupResourceClusters = false;
 }
 
 void UMapBuildDataRegistry::InvalidateReflectionCaptures(const TSet<FGuid>* ResourcesToKeep)
@@ -856,7 +858,6 @@ void UMapBuildDataRegistry::EmptyLevelData(const TSet<FGuid>* ResourcesToKeep)
 	}
 
 	LightmapResourceClusters.Empty();
-	bSetupResourceClusters = false;
 }
 
 FUObjectAnnotationSparse<FMeshMapBuildLegacyData, true> GComponentsWithLegacyLightmaps;
