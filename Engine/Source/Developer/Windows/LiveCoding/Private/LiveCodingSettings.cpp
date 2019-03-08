0da1e8572f6d787c5044007251a418e830e0d681
Ben Marsh <Ben.Marsh@epicgames.com>
2019-03-08 17:03:57 -0500

    LiveCoding: Make properties for toggling on/off for engine modules hidden in installed builds.
    #rb none
    #jira UE-71274
    
    [CL 5353250 by Ben Marsh in 4.22 branch]

--- Engine/Source/Developer/Windows/LiveCoding/Private/LiveCodingSettings.cpp
+++ Engine/Source/Developer/Windows/LiveCoding/Private/LiveCodingSettings.cpp
@@ -1,9 +1,22 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "LiveCodingSettings.h"
+#include "Misc/App.h"
 
 ULiveCodingSettings::ULiveCodingSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+	UProperty* EngineModulesProperty = StaticClass()->FindPropertyByName("bIncludeEngineModules");
+	check(EngineModulesProperty != nullptr);
+
+	UProperty* EnginePluginModulesProperty = StaticClass()->FindPropertyByName("bIncludeEnginePluginModules");
+	check(EnginePluginModulesProperty != nullptr);
+
+	if (FApp::IsEngineInstalled())
+	{
+		EngineModulesProperty->ClearPropertyFlags(CPF_Edit);
+		EnginePluginModulesProperty->ClearPropertyFlags(CPF_Edit);
+	}
+
 	bIncludeProjectModules = true;
 	bIncludeProjectPluginModules = true;
 }
