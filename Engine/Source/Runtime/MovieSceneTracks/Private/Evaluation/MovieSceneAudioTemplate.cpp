9d5be5d50781e1b69b6aa62f426a40678dbb8455
Max Chen <Max.Chen@epicgames.com>
2019-03-12 21:54:50 -0400

    Sequencer: Fix audio play clamping at incorrect sound duration
    #jira UE-69411
    #rb none
    
    [CL 5377115 by Max Chen in 4.22 branch]

--- Engine/Source/Runtime/MovieSceneTracks/Private/Evaluation/MovieSceneAudioTemplate.cpp
+++ Engine/Source/Runtime/MovieSceneTracks/Private/Evaluation/MovieSceneAudioTemplate.cpp
@@ -346,7 +346,7 @@ void FMovieSceneAudioSectionTemplateData::EnsureAudioIsPlaying(UAudioComponent& 
 		}
 
 		const float AudioTime = (Context.GetTime() / Context.GetFrameRate()) - SectionStartTimeSeconds + (float)Context.GetFrameRate().AsSeconds(AudioStartOffset);
-		if (AudioTime > 0.f && AudioTime < AudioComponent.Sound->Duration)
+		if (AudioTime >= 0.f && AudioTime < AudioComponent.Sound->GetDuration())
 		{
 			AudioComponent.Play(AudioTime);
 		}
@@ -411,3 +411,5 @@ void FMovieSceneAudioSectionTemplate::TearDown(FPersistentEvaluationData& Persis
 		TrackData.StopSoundsOnRow(AudioData.RowIndex);
 	}
 }
+
+PRAGMA_ENABLE_OPTIMIZATION
