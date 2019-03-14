7adc18db16a2860727ee85822043e721fd45500f
Krzysztof Narkowicz <Krzysztof.Narkowicz@epicgames.com>
2019-03-14 09:30:49 -0400

    Recreate uniform expression uniform buffer when material instance resource is initialized. This forced uniform expression UB to be recreated and fixes the check that we are trying to update uniform expression UB which doesn't match current layout. #rb none #jira UE-71222
    [CL 5397062 by Krzysztof Narkowicz in 4.22 branch]

--- Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
+++ Engine/Source/Runtime/Engine/Private/Materials/MaterialInstance.cpp
@@ -483,7 +483,7 @@ void UMaterialInstance::InitResources()
 
 	PropagateDataToMaterialProxy();
 
-	CacheMaterialInstanceUniformExpressions(this);
+	CacheMaterialInstanceUniformExpressions(this, true);
 }
 
 const UMaterial* UMaterialInstance::GetMaterial() const
