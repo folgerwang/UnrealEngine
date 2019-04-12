676b527fd2207a545b0ce8c920a2d8134d4f73b7
Lauren Ridge <Lauren.Ridge@epicgames.com>
2019-04-12 16:06:50 -0400

    Fix for crash when level list contains unloaded levels #jira UE-72729 #rb Jason.Stasik #lockdown Cristina.Riveron
    [CL 5869857 by Lauren Ridge in 4.22 branch]

--- Engine/Source/Editor/WorldBrowser/Private/WorldBrowserModule.cpp
+++ Engine/Source/Editor/WorldBrowser/Private/WorldBrowserModule.cpp
@@ -44,17 +44,20 @@ void FWorldBrowserModule::BuildLevelMenu(FMenuBuilder& MenuBuilder)
 		FLevelModelList ModelList = Model->GetFilteredLevels();
 		for (TSharedPtr<FLevelModel> LevelModel : ModelList)
 		{
-			FUIAction Action(FExecuteAction::CreateRaw(this, &FWorldBrowserModule::SetCurrentSublevel, LevelModel->GetLevelObject()),
-				FCanExecuteAction(),
-				FIsActionChecked::CreateRaw(this, &FWorldBrowserModule::IsCurrentSublevel, LevelModel->GetLevelObject()));
-			MenuBuilder.AddMenuEntry(FText::FromString(LevelModel->GetDisplayName()), FText::GetEmpty(), FSlateIcon(), Action, NAME_None, EUserInterfaceActionType::Button);
+			if (LevelModel->GetLevelObject())
+			{
+				FUIAction Action(FExecuteAction::CreateRaw(this, &FWorldBrowserModule::SetCurrentSublevel, LevelModel->GetLevelObject()),
+					FCanExecuteAction(),
+					FIsActionChecked::CreateRaw(this, &FWorldBrowserModule::IsCurrentSublevel, LevelModel->GetLevelObject()));
+				MenuBuilder.AddMenuEntry(FText::FromString(LevelModel->GetDisplayName()), FText::GetEmpty(), FSlateIcon(), Action, NAME_None, EUserInterfaceActionType::Button);
+			}
 		}
 	}
 }
 
 bool FWorldBrowserModule::IsCurrentSublevel(ULevel* InLevel)
 {
-	return InLevel->IsCurrentLevel();
+	return InLevel ? InLevel->IsCurrentLevel() : false;
 }
 
 void FWorldBrowserModule::SetCurrentSublevel(ULevel* InLevel)
