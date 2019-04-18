94f71d36b6ed9f642ea4746b8576885484df6197
Rolando Caloca <Rolando.Caloca@epicgames.com>
2019-04-18 08:59:51 -0400

    Disable Vulkan tessellation #rb none #rnx #jira UE-72991
    [CL 5980829 by Rolando Caloca in 4.22 branch]

--- Engine/Source/Runtime/RHI/Private/RHI.cpp
+++ Engine/Source/Runtime/RHI/Private/RHI.cpp
@@ -796,7 +796,7 @@ RHI_API bool RHISupportsTessellation(const EShaderPlatform Platform)
 {
 	if (IsFeatureLevelSupported(Platform, ERHIFeatureLevel::SM5))
 	{
-		return (Platform == SP_PCD3D_SM5) || (Platform == SP_XBOXONE_D3D12) || (Platform == SP_OPENGL_SM5) || (Platform == SP_OPENGL_ES31_EXT) || (Platform == SP_METAL_SM5) || (IsVulkanSM5Platform(Platform));
+		return (Platform == SP_PCD3D_SM5) || (Platform == SP_XBOXONE_D3D12) || (Platform == SP_OPENGL_SM5) || (Platform == SP_OPENGL_ES31_EXT) || (Platform == SP_METAL_SM5) /*|| (IsVulkanSM5Platform(Platform))*/;
 	}
 	return false;
 }
