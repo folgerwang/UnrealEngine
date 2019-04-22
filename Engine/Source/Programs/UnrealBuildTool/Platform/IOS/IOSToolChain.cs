42dbc9c5d2a8d4cedeca94217755038c2881d0c0
Cosmin Sulea <cosmin.sulea@xa.epicgames.com>
2019-04-22 04:24:21 -0400

    UE-71914 - Packaging code projects in Shipping configuration from Mac fails if there is a space in the project path #jira UE-71914 #rb Jack.Porter #lockdown Cristina.Riveron
    [CL 6032622 by Cosmin Sulea in 4.22 branch]

--- Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
+++ Engine/Source/Programs/UnrealBuildTool/Platform/IOS/IOSToolChain.cs
@@ -492,7 +492,7 @@ namespace UnrealBuildTool
 				FileItem OutputFile = FileItem.GetItemByFileReference(LinkEnvironment.OutputFilePath);
 
 				Result += " -fembed-bitcode -Xlinker -bitcode_verify -Xlinker -bitcode_hide_symbols -Xlinker -bitcode_symbol_map ";
-				Result += " -Xlinker " + Path.GetDirectoryName(OutputFile.AbsolutePath);
+				Result += " -Xlinker \"" + Path.GetDirectoryName(OutputFile.AbsolutePath) + "\"";
 			}
 
 			Result += " -dead_strip";
@@ -848,7 +848,7 @@ namespace UnrealBuildTool
 
 				linkCommandArguments += string.Format("rm -f \"{0}\";", OutputFile.AbsolutePath);
 				linkCommandArguments += string.Format("rm -f \"{0}\\*.bcsymbolmap\";", Path.GetDirectoryName(OutputFile.AbsolutePath));
-				linkCommandArguments += LinkerPath + " " + LinkCommandArguments + ";";
+				linkCommandArguments += "\"" + LinkerPath + "\" " + LinkCommandArguments + ";";
 
 				linkCommandArguments += "'";
 
@@ -857,7 +857,7 @@ namespace UnrealBuildTool
 			else
 			{
 				// This is not a shipping build so no need to delete the output file since symbols will not have been stripped from it.
-				LinkAction.CommandArguments = string.Format("-c '{0} {1}'", LinkerPath, LinkCommandArguments);
+				LinkAction.CommandArguments = string.Format("-c '\"{0}\" {1}'", LinkerPath, LinkCommandArguments);
 			}
 			Actions.Add(LinkAction);
 
