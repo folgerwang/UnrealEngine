8f95c1f82ccf0641c0aceed0adf27c9d6f201d2c
Aaron McLeran <Aaron.McLeran@epicgames.com>
2019-06-04 20:02:29 -0400

    Fix for submix effects not registering until restart.
    #rb Ethan.Geller, Maxwell.Hayes
    #jira UE-75595
    #lockdown Cristina.Riveron
    
    [CL 6843353 by Aaron McLeran in 4.22 branch]

--- Engine/Source/Runtime/AudioMixer/Private/AudioMixerDevice.cpp
+++ Engine/Source/Runtime/AudioMixer/Private/AudioMixerDevice.cpp
@@ -952,32 +952,42 @@ namespace Audio
 
 			if (!IsMasterSubmixType(InSoundSubmix))
 			{
+				FMixerSubmixPtr MixerSubmix; 
+
 				// If the sound submix wasn't already registered get it into the system.
 				if (!Submixes.Contains(InSoundSubmix))
 				{
-					FMixerSubmixPtr MixerSubmix = FMixerSubmixPtr(new FMixerSubmix(this));
+					MixerSubmix = FMixerSubmixPtr(new FMixerSubmix(this));
 					Submixes.Add(InSoundSubmix, MixerSubmix);
+				}
+				else
+				{
+					FMixerSubmixPtr* ExistingMixerSubmix = Submixes.Find(InSoundSubmix);
+					check(ExistingMixerSubmix);
+					MixerSubmix = *ExistingMixerSubmix;
+				}
 
-					if (bInit)
+				check(MixerSubmix.IsValid());
+
+				if (bInit)
+				{
+					// Setup the parent-child relationship
+					FMixerSubmixWeakPtr ParentSubmixInstance;
+					if (InSoundSubmix->ParentSubmix)
 					{
-						// Setup the parent-child relationship
-						FMixerSubmixWeakPtr ParentSubmixInstance;
-						if (InSoundSubmix->ParentSubmix)
-						{
-							ParentSubmixInstance = GetSubmixInstance(InSoundSubmix->ParentSubmix);
-						}
-						else
-						{
-							ParentSubmixInstance = GetMasterSubmix();
-						}
+						ParentSubmixInstance = GetSubmixInstance(InSoundSubmix->ParentSubmix);
+					}
+					else
+					{
+						ParentSubmixInstance = GetMasterSubmix();
+					}
 
-						FMixerSubmixPtr ParentSubmixInstancePtr = ParentSubmixInstance.Pin();
-						if (ParentSubmixInstancePtr.IsValid())
-						{
-							ParentSubmixInstancePtr->AddChildSubmix(MixerSubmix);
-							MixerSubmix->SetParentSubmix(ParentSubmixInstance);
-							MixerSubmix->Init(InSoundSubmix);
-						}
+					FMixerSubmixPtr ParentSubmixInstancePtr = ParentSubmixInstance.Pin();
+					if (ParentSubmixInstancePtr.IsValid())
+					{
+						ParentSubmixInstancePtr->AddChildSubmix(MixerSubmix);
+						MixerSubmix->SetParentSubmix(ParentSubmixInstance);
+						MixerSubmix->Init(InSoundSubmix);
 					}
 				}
 			}
