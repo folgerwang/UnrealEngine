e5c8952082d3a814bb2e2c543a6843dc1f57e5af
Aaron McLeran <Aaron.McLeran@epicgames.com>
2019-06-05 13:53:39 -0400

    Crash fix for listener transform update.
    #jira UE-75696
    #rb Ethan.Geller
    #lockdown Cristina.Riveron
    
    [CL 6851120 by Aaron McLeran in 4.22 branch]

--- Engine/Source/Runtime/AudioMixer/Private/AudioMixerDevice.cpp
+++ Engine/Source/Runtime/AudioMixer/Private/AudioMixerDevice.cpp
@@ -349,15 +349,6 @@ namespace Audio
 			AudioMixerPlatform->ResumePlaybackOnNewDevice();
 		}
 
-		ListenerTransforms.Reset();
-		for (FListener& Listener : Listeners)
-		{
-			ListenerTransforms.Add(Listener.Transform);
-		}
-
-		// Update listener transforms, some effects use the listener transform data
-		SourceManager.SetListenerTransforms(ListenerTransforms);
-
 		// Loop through any envelope-following submixes and perform any broadcasting of envelope data if needed
 		TArray<float> SubmixEnvelopeData;
 		for (USoundSubmix* SoundSubmix : EnvelopeFollowingSubmixes)
